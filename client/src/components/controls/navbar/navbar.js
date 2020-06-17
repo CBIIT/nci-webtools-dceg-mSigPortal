dec4f33fd9ea90e152a443c8202db4bdb8f496b8
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-06-17 17:37:25 -0400

    use container width

--- client/src/components/controls/navbar/navbar.js
+++ client/src/components/controls/navbar/navbar.js
@@ -5,29 +5,27 @@ import './navbar.scss';
 export default function Navbar({ links }) {
   return (
     <div className="bg-primary text-white shadow-sm">
-      <div className="px-0">
-        <div className="ml-3">
-          {[{ route: '/', title: 'Home', exact: true }]
-            .concat(links)
-            .filter((link) => link.title)
-            .sort((a, b) => a.navIndex - b.navIndex)
-            .map(({ route, action, title, exact }, index) => (
-              <div data-testid="Navbar" className="d-inline-block" key={title}>
-                <NavLink
-                  data-testid={`Navbar-NavLink-${index}`}
-                  // key={title}
-                  className="navlinks py-2 px-4 d-inline-block"
-                  activeClassName="active-navlinks"
-                  exact={exact}
-                  to={route}
-                >
-                  {title}
-                </NavLink>
-                <div className="d-lg-none w-100"></div>
-              </div>
-            ))}
-          {/* <pre>{JSON.stringify(links)}</pre> */}
-        </div>
+      <div className="container">
+        {[{ route: '/', title: 'Home', exact: true }]
+          .concat(links)
+          .filter((link) => link.title)
+          .sort((a, b) => a.navIndex - b.navIndex)
+          .map(({ route, action, title, exact }, index) => (
+            <div data-testid="Navbar" className="d-inline-block" key={title}>
+              <NavLink
+                data-testid={`Navbar-NavLink-${index}`}
+                // key={title}
+                className="navlinks py-2 px-4 d-inline-block"
+                activeClassName="active-navlinks"
+                exact={exact}
+                to={route}
+              >
+                {title}
+              </NavLink>
+              <div className="d-lg-none w-100"></div>
+            </div>
+          ))}
+        {/* <pre>{JSON.stringify(links)}</pre> */}
       </div>
     </div>
   );
