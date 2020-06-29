da4315b5d302625549137d599240211d006c3b7c
kvnjng <kvvnjng@gmail.com>
2020-06-29 16:49:38 -0400

    depreciate old store and reducer code

--- client/src/services/visualSlice.js
+++ client/src/services/visualSlice.js
@@ -1,5 +1,26 @@
 import { createSlice,configureStore} from '@reduxjs/toolkit';
-import { getInitialState } from './store'
+// import { getInitialState } from './store-old'
+
+export const getInitialState = () => ({
+    visualize: {
+      count: 0,
+      inputFormat: 'vcf',
+      inputFile: null,
+      selectedGenome: 'GRCh37',
+      experimentalStrategy: 'WGS',
+      mutationSplit: 'False',
+      isMultiple: false,
+      collapseSample: 'False',
+      mutationFilter: '',
+      queueMode: false,
+      email: ''
+    },
+    visualizeResults: {
+      uid: null,
+      mapping: null,
+      displayedPlot: null
+    }
+  });
 
 const visualSlice = createSlice({
     name: 'visualize',
@@ -15,6 +36,9 @@ const visualSlice = createSlice({
   })
 
 const {actions,reducer} = visualSlice
-export const store = configureStore({reducer: reducer})
+export const store = configureStore({
+    reducer: reducer,
+    preloadedState: getInitialState()
+})
 export const {setInputFormat} = actions
 // export default store
