1a8ed50abea85d57a1387a12ab88db1a4ed5a640
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-07-09 13:50:35 -0400

    log error message

--- server/msigportal-server.js
+++ server/msigportal-server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve('www')));
 app.use(express.json());
 
 app.use((err, req, res, next) => {
-  console.error(err.message);
+  logger.error('Caught Error:\n' + err.message);
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
