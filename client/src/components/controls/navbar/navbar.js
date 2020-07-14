ad90f5123983e60d7cb639177745aa313c617e88
bchen49 <benchen4998@gmail.com>
2020-07-14 11:26:49 -0400

    Add vertical navbar for small screens

--- client/src/components/controls/navbar/navbar.js
+++ client/src/components/controls/navbar/navbar.js
@@ -5,7 +5,7 @@ import './navbar.scss';
 export function Navbar({ links }) {
   return (
     <div className="bg-dark text-white gradient shadow-sm">
-      <div className="container d-flex justify-content-center">
+      <div className="container d-none d-md-flex justify-content-center">
         {[{ route: '/', title: 'Home', exact: true }]
           .concat(links)
           .filter((link) => link.title)
@@ -27,6 +27,29 @@ export function Navbar({ links }) {
             </div>
           ))}
         {/* <pre>{JSON.stringify(links)}</pre> */}
+      </div>
+
+      <div className="container d-flex flex-column d-md-none" align="center">
+        {[{ route: '/', title: 'Home', exact: true }]
+            .concat(links)
+            .filter((link) => link.title)
+            .sort((a, b) => a.navIndex - b.navIndex)
+            .map(({ route, action, title, exact }, index) => (
+              <div data-testid="Navbar" className="d-block" key={title}>
+                <NavLink
+                  data-testid={`Navbar-NavLink-${index}`}
+                  id={title}
+                  // key={title}
+                  className="navlinks py-2 px-4 d-inline-block"
+                  activeClassName="active-navlinks"
+                  exact={exact}
+                  to={route}
+                >
+                  {title}
+                </NavLink>
+               
+              </div>
+          ))}
       </div>
     </div>
   );
