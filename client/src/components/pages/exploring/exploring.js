e1109c73b62e68e551157efb6c9ac48b09aff974
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-09-22 18:06:35 -0400

    add sidebar

--- client/src/components/pages/exploring/exploring.js
+++ client/src/components/pages/exploring/exploring.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
-import { Card, Nav } from 'react-bootstrap';
+import { Card, Nav, Row, Col, Form } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import {
+  SidebarContainer,
+  SidebarPanel,
+  MainPanel,
+} from '../../controls/sidebar-container/sidebar-container';
 import SignatureExploring from './signatureExploring';
 import {
-  dispatchError,
   dispatchExploring,
   dispatchExpMutationalProfiles,
   dispatchExpCosineSimilarity,
@@ -12,16 +16,21 @@ import {
 
 const { Header, Body } = Card;
 const { Item, Link } = Nav;
+const { Group, Label, Check } = Form;
 
 export default function Explore() {
   const rootURL = window.location.pathname;
-
-  const { displayTab, projectID, refSigData } = useSelector(
-    (state) => state.exploring
-  );
+  const {
+    displayTab,
+    projectID,
+    refSigData,
+    source,
+    openSidebar,
+    submitted,
+  } = useSelector((state) => state.exploring);
 
   useEffect(() => {
-    if (!refSigData.size) getInitalRefSigData();
+    if (!Object.keys(refSigData).length) getInitalRefSigData();
   }, []);
 
   async function getReferenceSignatureData(filters = {}) {
@@ -165,46 +174,108 @@ export default function Explore() {
   }
 
   return (
-    <div className="position-relative m-3">
-      <Card>
-        <Header>
-          <Nav variant="pills" defaultActiveKey="#mutationalProfiles">
-            {[
-              { title: 'Signature Exploring', id: 'signatureExploring' },
-              { title: 'Exposure Exploring', id: 'exposureExploring' },
-            ].map(({ title, id }) => {
-              return (
-                <Item key={id}>
-                  <Link
-                    active={displayTab == id}
-                    onClick={() => dispatchExploring({ displayTab: id })}
-                  >
-                    {title}
-                  </Link>
-                </Item>
-              );
-            })}
-          </Nav>
-        </Header>
-        <Body
-          style={{
-            display: displayTab == 'signatureExploring' ? 'block' : 'none',
-          }}
-        >
-          <SignatureExploring
-            getReferenceSignatureData={(columns, filters) =>
-              getReferenceSignatureData(columns, filters)
-            }
-          />
-        </Body>
-        {/* <Body
+    <div className="position-relative">
+      <SidebarContainer
+        className="m-3"
+        collapsed={!openSidebar}
+        onCollapsed={(e) => dispatchExploring({ openSidebar: !e })}
+      >
+        <SidebarPanel>
+          <div className="p-3 shadow-sm bg-white">
+            <Row>
+              <Col sm="auto">
+                <Group className="d-flex">
+                  <Label className="mr-auto">
+                    <h3 className="mb-2">Data Source</h3>
+                  </Label>
+                  <Check inline id="radioUser" className="ml-4">
+                    <Check.Input
+                      disabled={submitted}
+                      type="radio"
+                      value="user"
+                      checked={source == 'user'}
+                      onChange={(e) => dispatchExploring({ source: 'user' })}
+                    />
+                    <Check.Label className="font-weight-normal">
+                      User
+                    </Check.Label>
+                  </Check>
+                  <Check inline id="radioPublic">
+                    <Check.Input
+                      disabled={submitted}
+                      type="radio"
+                      value="public"
+                      checked={source == 'public'}
+                      onChange={(e) => dispatchExploring({ source: 'public' })}
+                    />
+                    <Check.Label className="font-weight-normal">
+                      Public
+                    </Check.Label>
+                  </Check>
+                </Group>
+              </Col>
+            </Row>
+            <Row style={{ display: source == 'user' ? 'block' : 'none' }}>
+              <Col sm="auto" className="w-100">
+                {/* <UploadForm /> */}
+              </Col>
+            </Row>
+            <Row style={{ display: source == 'public' ? 'block' : 'none' }}>
+              <Col sm="auto" className="w-100">
+                {/* <PublicForm /> */}
+              </Col>
+            </Row>
+          </div>
+          <hr className="d-lg-none" style={{ opacity: 0 }}></hr>
+        </SidebarPanel>
+        <MainPanel>
+          <div
+            className="p-3 shadow-sm bg-white"
+            style={{ minHeight: '420px' }}
+          >
+            <Card>
+              <Header>
+                <Nav variant="pills" defaultActiveKey="#mutationalProfiles">
+                  {[
+                    { title: 'Signature Exploring', id: 'signatureExploring' },
+                    { title: 'Exposure Exploring', id: 'exposureExploring' },
+                  ].map(({ title, id }) => {
+                    return (
+                      <Item key={id}>
+                        <Link
+                          active={displayTab == id}
+                          onClick={() => dispatchExploring({ displayTab: id })}
+                        >
+                          {title}
+                        </Link>
+                      </Item>
+                    );
+                  })}
+                </Nav>
+              </Header>
+              <Body
+                style={{
+                  display:
+                    displayTab == 'signatureExploring' ? 'block' : 'none',
+                }}
+              >
+                <SignatureExploring
+                  getReferenceSignatureData={(columns, filters) =>
+                    getReferenceSignatureData(columns, filters)
+                  }
+                />
+              </Body>
+              {/* <Body
           style={{
             display: displayTab == 'exposureExploring' ? 'block' : 'none',
           }}
         >
           <MutationalProfiles />
         </Body> */}
-      </Card>
+            </Card>
+          </div>
+        </MainPanel>
+      </SidebarContainer>
     </div>
   );
 }
