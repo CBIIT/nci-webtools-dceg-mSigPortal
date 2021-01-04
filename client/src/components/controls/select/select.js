0ace8ba661eaefc9e83baa14f513ac9b0f167ab1
Phyllip Cho <phyllip3@users.noreply.github.com>
2021-01-04 11:38:14 -0500

    remove htmlFor attribute

--- client/src/components/controls/select/select.js
+++ client/src/components/controls/select/select.js
@@ -24,7 +24,7 @@ export default function Select({
 
   return (
     <Group controlId={id} className={className}>
-      <Label htmlFor={id}>{label}</Label>
+      <Label>{label}</Label>
       <ReactSelect
         inputId={id}
         options={options}
