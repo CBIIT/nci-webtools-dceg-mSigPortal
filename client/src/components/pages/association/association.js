414065ee750fcb83744821bc3fc2ba3ab71bba4a
Phyllip Cho <phyllip3@users.noreply.github.com>
2021-02-23 16:40:26 -0500

    update page styles

--- client/src/components/pages/association/association.js
+++ client/src/components/pages/association/association.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 export default function Association() {
   return (
-    <div
-      className="mt-3 container bg-white tab-pane-bordered rounded-0 p-4"
-      style={{
-        minHeight: '420px',
-      }}
-    >
-      <h1 className="font-weight-light">Association</h1>
-      <hr />
-
-      <p>TBA</p>
+    <div className="mx-3">
+      <div className="bg-white border p-3 mx-3" style={{ minHeight: '420px' }}>
+        <p>TBA</p>
+      </div>
     </div>
   );
 }
