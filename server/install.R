2d292eb55daaf38ef9cd0bf073c38f46a3290ae9
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-02-02 13:14:36 -0500

    halt install on failure

--- server/install.R
+++ server/install.R
@@ -4,6 +4,9 @@ Sys.setenv(
   "R_REMOTES_NO_ERRORS_FROM_WARNINGS" = TRUE
 )
 
+# warning from failing to install are turned into errors, halting the entire install
+options(warn = 2)
+
 install.packages(
   c(
     'aws.ec2metadata',
