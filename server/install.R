eca779b12c896378d45de3d44623b8d9f226ff1e
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-02-02 13:36:33 -0500

    stop install on warning

--- server/install.R
+++ server/install.R
@@ -5,47 +5,51 @@ Sys.setenv(
 )
 
 # warning from failing to install are turned into errors, halting the entire install
-options(warn = 2)
+# options(warn = 2)
 
-install.packages(
-  c(
-    'aws.ec2metadata',
-    'aws.s3',
-    'BiocManager',
-    'car',
-    'coop',
-    'cowplot',
-    'entropy',
-    'factoextra',
-    'FactoMineR',
-    'ggplot2',
-    'ggdendro',
-    'ggExtra',
-    'ggforce',
-    'ggpubr',
-    'ggrepel',
-    'ggridges',
-    'ggsci',
-    'ggside',
-    'ggstatsplot',
-    'ggtext',
-    'hrbrthemes',
-    'janitor',
-    'scales',
-    'svglite',
-    'tidyverse',
-    'PMCMRplus'
-  ),
-  repos = 'https://cloud.r-project.org/'
-)
+tryCatch({
+  install.packages(
+    c(
+        'aws.ec2metadata',
+        'aws.s3',
+        'BiocManager',
+        'car',
+        'coop',
+        'cowplot',
+        'entropy',
+        'factoextra',
+        'FactoMineR',
+        'ggplot2',
+        'ggdendro',
+        'ggExtra',
+        'ggforce',
+        'ggpubr',
+        'ggrepel',
+        'ggridges',
+        'ggsci',
+        'ggside',
+        'ggstatsplot',
+        'ggtext',
+        'hrbrthemes',
+        'janitor',
+        'scales',
+        'svglite',
+        'tidyverse',
+        'PMCMRplus'
+    ),
+    repos = 'https://cloud.r-project.org/'
+    )
 
-BiocManager::install(
-  c(
-    'BSgenome.Hsapiens.UCSC.hg19',
-    'BSgenome.Hsapiens.UCSC.hg38',
-    'ChIPseeker',
-    'org.Hs.eg.db',
-    'TxDb.Hsapiens.UCSC.hg19.knownGene',
-    'TxDb.Hsapiens.UCSC.hg38.knownGene'
-  )
-)
+  BiocManager::install(
+    c(
+        'BSgenome.Hsapiens.UCSC.hg19',
+        'BSgenome.Hsapiens.UCSC.hg38',
+        'ChIPseeker',
+        'org.Hs.eg.db',
+        'TxDb.Hsapiens.UCSC.hg19.knownGene',
+        'TxDb.Hsapiens.UCSC.hg38.knownGene'
+    )
+    )
+}, warning = function(e) {
+  stop(e)
+})
