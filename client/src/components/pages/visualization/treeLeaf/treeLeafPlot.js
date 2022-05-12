8fc3a62b1027ba528be3ac5c753cca7b00b65fcb
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-05-12 15:33:11 -0400

    add colorscale legend

--- client/src/components/pages/visualization/treeLeaf/treeLeafPlot.js
+++ client/src/components/pages/visualization/treeLeaf/treeLeafPlot.js
@@ -9,7 +9,9 @@ export default function D3TreeLeaf({ width = 1000, height = 1000, ...props }) {
   const { hierarchy, attributes } = cloneDeep(useRecoilValue(getGraphData));
   const form = useRecoilValue(formState);
 
-  const nodeRef = useRef(null);
+  const plotRef = useRef(null);
+  const csLegendRef = useRef(null);
+  // const mutationsLegendRef = useRef(null);
 
   const groupAttributesBySample = attributes.reduce(
     (map, e) => ((map[e.Sample] = e), map),
@@ -17,24 +19,35 @@ export default function D3TreeLeaf({ width = 1000, height = 1000, ...props }) {
   );
 
   useEffect(() => {
-    if (nodeRef.current && hierarchy) {
+    if (plotRef.current && hierarchy) {
       // const treeLeaf = createRadialForce(hierarchy, groupAttributesBySample, {
       //   width,
       //   height,
       // });
 
-      const plot = createRadialTree(hierarchy, groupAttributesBySample, {
-        label: (d) => (form.showLabels ? d.name : ''),
-        width,
-        height,
-      });
+      const [plot, csLegend, mutationsLegend] = createRadialTree(
+        hierarchy,
+        groupAttributesBySample,
+        {
+          label: (d) => (form.showLabels ? d.name : ''),
+          width,
+          height,
+        }
+      );
 
-      nodeRef.current.replaceChildren(plot);
-      // tooltipRef.current.replaceChildren(tooltip);
+      plotRef.current.replaceChildren(plot);
+      csLegendRef.current.replaceChildren(csLegend);
+      // mutationsLegendRef.current.replaceChildren(mutationsLegend);
     }
-  }, [hierarchy, nodeRef, width, height]);
+  }, [hierarchy, plotRef, width, height]);
 
-  return <div className="border rounded" ref={nodeRef} {...props} />;
+  return (
+    <div className="border rounded p-3" {...props}>
+      <div ref={csLegendRef} />
+      {/* <div ref={mutationsLegendRef} /> */}
+      <div ref={plotRef} />
+    </div>
+  );
 }
 
 // export function createRadialForce(
@@ -218,15 +231,9 @@ function createRadialTree(
       width - marginLeft - marginRight,
       height - marginTop - marginBottom
     ) / 2, // outer radius
-    r = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(Object.values(attributes).map(({ Mutations }) => Mutations)),
-      ])
-      .range([2, 20]), // radius of nodes
+    r = d3.scaleSqrt().range([4, 20]), // radius of nodes
     padding = 1, // horizontal padding for first and last column
-    fill = d3.scaleSequential(d3.interpolateRdYlGn), // fill for nodes
+    fill = d3.scaleSequential(d3.interpolatePRGn), // fill for nodes
     fillOpacity, // fill opacity for nodes
     stroke = '#555', // stroke for links
     strokeWidth = 1.5, // stroke width for links
@@ -263,7 +270,7 @@ function createRadialTree(
   const zoom = d3.zoom().scaleExtent([1, 5]).on('zoom', zoomed);
 
   function zoomed({ transform }) {
-    d3.selectAll('g').attr('transform', transform);
+    d3.selectAll('#plot').attr('transform', transform);
   }
 
   const container = d3.create('div');
@@ -280,6 +287,7 @@ function createRadialTree(
   // add lines
   svg
     .append('g')
+    .attr('id', 'plot')
     .attr('fill', 'none')
     .attr('stroke', stroke)
     .attr('stroke-opacity', strokeOpacity)
@@ -300,6 +308,7 @@ function createRadialTree(
   // add sample nodes
   const node = svg
     .append('g')
+    .attr('id', 'plot')
     .selectAll('a')
     .data(root.descendants())
     .join('a')
@@ -310,22 +319,36 @@ function createRadialTree(
       (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
     );
 
-  // set size and color
+  // gather range of attributes
+  const mutations = Object.values(attributes).map((e) => e.Mutations);
+  const cs = Object.values(attributes).map((e) => e.Cosine_similarity);
+  const mutationMin = d3.min(mutations);
+  const mutationMax = d3.max(mutations);
+  const csMin = d3.min(cs);
+  const csMax = d3.max(cs);
+
+  // set size and color of nodes
   node
     .append('circle')
     .attr('fill', ({ data }) =>
       data.name && attributes[data.name]
-        ? fill(attributes[data.name].Cosine_similarity)
-        : 'stroke'
+        ? fill.domain([csMin, csMax])(attributes[data.name].Cosine_similarity)
+        : stroke
     )
     .attr('r', ({ data }) =>
       data.name && attributes[data.name]
-        ? r(attributes[data.name].Mutations)
+        ? r.domain([mutationMin, mutationMax])(attributes[data.name].Mutations)
         : null
     )
     .on('mouseover', mouseover)
     .on('mousemove', mousemove)
     .on('mouseleave', mouseleave);
+
+  const csLegend = Legend(fill.domain([csMin, csMax]), {
+    title: 'Cosine Similarity',
+    // marginLeft: '1rem',
+    // marginTop: '1rem',
+  });
 
   // add tooltips
   const tooltip = container
@@ -378,5 +401,113 @@ function createRadialTree(
       .attr('stroke-width', haloWidth)
       .text((d, i) => L[i]);
 
-  return container.node();
+  return [container.node(), csLegend];
+}
+
+// Copyright 2021, Observable Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/color-legend
+function Legend(
+  color,
+  {
+    title,
+    tickSize = 6,
+    width = 320,
+    height = 44 + tickSize,
+    marginTop = 18,
+    marginRight = 0,
+    marginBottom = 16 + tickSize,
+    marginLeft = 0,
+    ticks = width / 64,
+    tickFormat,
+    tickValues,
+  } = {}
+) {
+  function ramp(color, n = 256) {
+    const canvas = document.createElement('canvas');
+    canvas.width = n;
+    canvas.height = 1;
+    const context = canvas.getContext('2d');
+    for (let i = 0; i < n; ++i) {
+      context.fillStyle = color(i / (n - 1));
+      context.fillRect(i, 0, 1, 1);
+    }
+    return canvas;
+  }
+
+  const svg = d3
+    .create('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', [0, 0, width, height])
+    .style('overflow', 'visible')
+    .style('display', 'block');
+
+  let tickAdjust = (g) =>
+    g.selectAll('.tick line').attr('y1', marginTop + marginBottom - height);
+  let x;
+
+  // Sequential
+  if (color.interpolator) {
+    x = Object.assign(
+      color
+        .copy()
+        .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+      {
+        range() {
+          return [marginLeft, width - marginRight];
+        },
+      }
+    );
+
+    svg
+      .append('image')
+      .attr('x', marginLeft)
+      .attr('y', marginTop)
+      .attr('width', width - marginLeft - marginRight)
+      .attr('height', height - marginTop - marginBottom)
+      .attr('preserveAspectRatio', 'none')
+      .attr('xlink:href', ramp(color.interpolator()).toDataURL());
+
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3
+          .range(n)
+          .map((i) => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== 'function') {
+        tickFormat = d3.format(tickFormat === undefined ? ',f' : tickFormat);
+      }
+    }
+  }
+
+  svg
+    .append('g')
+    .attr('id', `legend-${title.replace(/\W/g, '')}`)
+    .attr('transform', `translate(0,${height - marginBottom})`)
+    .call(
+      d3
+        .axisBottom(x)
+        .ticks(ticks, typeof tickFormat === 'string' ? tickFormat : undefined)
+        .tickFormat(typeof tickFormat === 'function' ? tickFormat : undefined)
+        .tickSize(tickSize)
+        .tickValues(tickValues)
+    )
+    .call(tickAdjust)
+    .call((g) => g.select('.domain').remove())
+    .call((g) =>
+      g
+        .append('text')
+        .attr('x', marginLeft)
+        .attr('y', marginTop + marginBottom - height - 6)
+        .attr('fill', 'currentColor')
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 'bold')
+        .attr('class', 'title')
+        .text(title)
+    );
+
+  return svg.node();
 }
