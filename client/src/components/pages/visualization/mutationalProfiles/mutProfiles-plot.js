4ce703dd476109f1030780423608f67feb5d704e
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-06-06 09:44:23 -0400

    adjust the y alignment on shapes and annotations for ID plot

--- client/src/components/pages/visualization/mutationalProfiles/mutProfiles-plot.js
+++ client/src/components/pages/visualization/mutationalProfiles/mutProfiles-plot.js
@@ -1,7 +1,7 @@
-import cloneDeep from 'lodash/cloneDeep';
-import { useRecoilValue } from 'recoil';
-import { getPlot } from './mutProfiles.state';
-import Plot from 'react-plotly.js';
+import cloneDeep from "lodash/cloneDeep";
+import { useRecoilValue } from "recoil";
+import { getPlot } from "./mutProfiles.state";
+import Plot from "react-plotly.js";
 
 export default function MutProfilePlot() {
   const { data, layout, config } = useRecoilValue(getPlot);
@@ -11,7 +11,7 @@ export default function MutProfilePlot() {
       {data.length ? (
         <Plot
           className="w-100"
-          style={{ height: '800px' }}
+          style={{ height: "500px" }}
           data={cloneDeep(data)}
           layout={cloneDeep(layout)}
           config={cloneDeep(config)}
