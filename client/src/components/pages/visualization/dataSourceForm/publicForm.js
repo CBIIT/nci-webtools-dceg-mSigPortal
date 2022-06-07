08dff747c20468d62fe50ca8f0cfe507eecc50b6
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-06-07 15:17:51 -0400

    recoil form wip

---
+++ client/src/components/pages/visualization/dataSourceForm/publicForm.js
@@ -0,0 +1,264 @@
+import React, { useEffect } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useRecoilState, useResetRecoilState } from 'recoil';
+// import { useForm, Controller } from 'react-hook-form';
+import Select from 'react-select';
+import { sourceFormState } from '../visualization.state';
+
+export default function PublicForm() {
+  const [formState, setForm] = useRecoilState(sourceFormState);
+  const mergeForm = (state) => setForm({ ...formState, ...state });
+  const reset = useResetRecoilState(sourceFormState);
+
+  const {
+    study,
+    studyOptions,
+    cancerType,
+    cancerTypeOptions,
+    pubExperimentalStrategy,
+    pubExperimentOptions,
+    pDataOptions,
+    submitted,
+    loading,
+    loadingPublic,
+    source,
+  } = formState;
+
+  // useEffect(() => {
+  //   if (!pDataOptions.length && !loadingPublic) getPublicDataOptions();
+  // }, [pDataOptions, source]);
+
+  function handleSelect(value, action) {
+    const { name } = action;
+    mergeForm({ [name]: value });
+  }
+
+  async function handleSubmit() {
+    const args = {
+      study: study,
+      cancerType: cancerType,
+      experimentalStrategy: pubExperimentalStrategy,
+    };
+
+    // mergeState({
+    //   loading: {
+    //     active: true,
+    //     // content: 'Loading Public Data',
+    //     // showIndicator: true,
+    //   },
+    // });
+    // try {
+    //   const response = await fetch(`api/visualizationWrapper`, {
+    //     method: 'POST',
+    //     headers: {
+    //       Accept: 'application/json',
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ fn: 'getPublicData', args }),
+    //   });
+
+    //   if (response.ok) {
+    //     const { output, projectID } = await response.json();
+    //     if (output.error) throw output.error;
+    //     if (output.uncaughtError) throw output.uncaughtError;
+
+    //     mergeState({
+    //       svgList: output,
+    //       projectID: projectID,
+    //     });
+    //   } else if (response.status == 504) {
+    //     mergeState({
+    //       error: 'Please Reset Your Parameters and Try again.',
+    //     });
+    //     mergeError({
+    //       visible: true,
+    //       message:
+    //         'Your submission has timed out. Please try again by submitting this job to a queue instead.',
+    //     });
+    //   } else {
+    //     mergeState({
+    //       error: 'Please Reset Your Parameters and Try again.',
+    //     });
+    //   }
+    // } catch (err) {
+    //   mergeError(err.message);
+    //   mergeState({
+    //     error: 'Please Reset Your Parameters and Try again.',
+    //   });
+    // }
+    // mergeState({ loading: { active: false } });
+  }
+
+  function handleReset() {
+    // const params = {
+    //   pDataOptions: pDataOptions,
+    //   studyOptions: studyOptions,
+    //   study: studyOptions[0],
+    //   cancerTypeOptions: cancerTypeOptions,
+    //   cancerType: 'Lung-AdenoCA',
+    //   pubExperimentOptions: pubExperimentOptions,
+    //   pubExperimentalStrategy: pubExperimentOptions[0],
+    // };
+    reset();
+    window.location.hash = '#/visualization';
+    // resetVisualization();
+    // mergeState(params);
+  }
+
+  async function getPublicDataOptions() {
+    // try {
+    //   // const pDataOptions = await getJSON(
+    //   //   `Others/json/Visualization-Public.json`
+    //   // );
+    //   const studyOptions = [...new Set(pDataOptions.map((data) => data.Study))];
+    //   // default study
+    //   const study = 'PCAWG';
+    //   const cancerTypeOptions = [
+    //     ...new Set(
+    //       pDataOptions
+    //         .filter((data) => data.Study == study)
+    //         .map((data) => data.Cancer_Type)
+    //     ),
+    //   ];
+    //   //  default cancer type
+    //   const cancer = 'Lung-AdenoCA';
+    //   const pubExperimentOptions = [
+    //     ...new Set(
+    //       pDataOptions
+    //         .filter((data) => data.Study == study && data.Cancer_Type == cancer)
+    //         .map((data) => data.Dataset)
+    //     ),
+    //   ];
+    //   mergeState({
+    //     pDataOptions: pDataOptions,
+    //     study: study,
+    //     studyOptions: studyOptions,
+    //     cancerType: cancer,
+    //     cancerTypeOptions: cancerTypeOptions,
+    //     pubExperimentalStrategy: pubExperimentOptions[0],
+    //     pubExperimentOptions: pubExperimentOptions,
+    //   });
+    //   mergeCosineSimilarity({
+    //     pubStudy: study,
+    //     pubCancerType: cancer,
+    //     pubCancerTypeOptions: cancerTypeOptions,
+    //   });
+    //   mergeProfileComparison({
+    //     pubStudy: study,
+    //     pubCancerType: cancer,
+    //     pubCancerTypeOptions: cancerTypeOptions,
+    //   });
+    //   mergePCA({
+    //     pubStudy: study,
+    //     pubCancerType: cancer,
+    //     pubCancerTypeOptions: cancerTypeOptions,
+    //   });
+    // } catch (err) {
+    //   mergeError(err.message);
+    // }
+  }
+
+  function handleStudyChange(study) {
+    const cancerTypeOptions = [
+      ...new Set(
+        pDataOptions
+          .filter((data) => data.Study == study)
+          .map((data) => data.Cancer_Type)
+      ),
+    ];
+
+    const esOptions = [
+      ...new Set(
+        pDataOptions
+          .filter(
+            (data) =>
+              data.Study == study && data.Cancer_Type == cancerTypeOptions[0]
+          )
+          .map((data) => data.Dataset)
+      ),
+    ];
+
+    // mergeState({
+    //   study: study,
+    //   cancerType: cancerTypeOptions[0],
+    //   cancerTypeOptions: cancerTypeOptions,
+    //   pubExperimentalStrategy: esOptions[0],
+    //   pubExperimentOptions: esOptions,
+    // });
+  }
+
+  function handleCancerChange(cancer) {
+    const pubExperimentOptions = [
+      ...new Set(
+        pDataOptions
+          .filter((data) => data.Study == study && data.Cancer_Type == cancer)
+          .map((data) => data.Dataset)
+      ),
+    ];
+
+    // mergeState({
+    //   cancerType: cancer,
+    //   pubExperimentalStrategy: pubExperimentOptions[0],
+    //   pubExperimentOptions: pubExperimentOptions,
+    // });
+  }
+
+
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Study</Form.Label>
+        <Select
+          className="mb-2"
+          name="study"
+          disabled={submitted}
+          value={study}
+          options={studyOptions}
+          onChange={handleSelect}
+        />
+      </Form.Group>
+      <Select
+        className="mb-2"
+        id="publicFromCancerType"
+        label="Cancer Type or Group"
+        disabled={submitted}
+        value={cancerType}
+        options={cancerTypeOptions}
+        onChange={handleSelect}
+      />
+      <Select
+        className="mb-4"
+        id="publicFormStrategy"
+        label="Experimental Strategy"
+        disabled={submitted}
+        value={pubExperimentalStrategy}
+        options={pubExperimentOptions}
+        onChange={handleSelect}
+      />
+
+      <Row>
+        <Col>
+          <Button
+            disabled={loading.active || loadingPublic}
+            className="w-100"
+            variant="secondary"
+            onClick={() => handleReset()}
+          >
+            Reset
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            disabled={loading.active || loadingPublic || submitted}
+            className="w-100"
+            variant="primary"
+            type="button"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
