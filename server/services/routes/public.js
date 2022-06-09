dbfc6bc4ee6d55d1c7444bb466eb9806a3374bc3
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-06-09 15:34:12 -0400

    enable cors for public api

--- server/services/routes/public.js
+++ server/services/routes/public.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const cors = require('cors');
 
 const router = Router();
 
+router.use(cors());
 router.get('/ping', (req, res) => res.send(true));
 
 module.exports = router;
