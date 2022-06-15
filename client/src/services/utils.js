d62ca1a74b52393b52d78c350366a302c1d47aad
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-06-15 18:11:01 -0400

    add utils

--- client/src/services/utils.js
+++ client/src/services/utils.js
@@ -19,6 +19,39 @@ export function defaultFilter(filterOptions) {
   return filterOptions.includes('NA') ? 'NA' : filterOptions[0];
 }
 
+export function defaultProfile2(profileOptions) {
+  const options = profileOptions.map(({ value }) => value);
+  if (options.includes('SBS')) return { label: 'SBS', value: 'SBS' };
+  if (options.includes('DBS')) return { label: 'DBS', value: 'DBS' };
+  if (options.includes('ID')) return { label: 'ID', value: 'ID' };
+}
+
+export function defaultMatrix2(profile, matrixOptions) {
+  const options = matrixOptions.map(({ value }) => value);
+
+  if (profile.value == 'SBS')
+    return options.includes('96')
+      ? { label: '96', value: '96' }
+      : matrixOptions[0];
+
+  if (profile.value == 'DBS')
+    return options.includes('78')
+      ? { label: '78', value: '78' }
+      : matrixOptions[0];
+
+  if (profile.value == 'ID')
+    return options.includes('83')
+      ? { label: '83', value: '83' }
+      : matrixOptions[0];
+}
+
+export function defaultFilter2(filterOptions) {
+  const options = filterOptions.map(({ value }) => value);
+  return options.includes('NA')
+    ? { label: 'NA', value: 'NA' }
+    : filterOptions[0];
+}
+
 export function getJSON(path) {
   return fetch(`web/getFileS3`, {
     method: 'POST',
--- server/services/utils.js
+++ server/services/utils.js
@@ -19,8 +19,44 @@ function defaultFilter(filterOptions) {
   return filterOptions.includes('NA') ? 'NA' : filterOptions[0];
 }
 
+function defaultProfile2(profileOptions) {
+  const options = profileOptions.map(({ value }) => value);
+  if (options.includes('SBS')) return { label: 'SBS', value: 'SBS' };
+  if (options.includes('DBS')) return { label: 'DBS', value: 'DBS' };
+  if (options.includes('ID')) return { label: 'ID', value: 'ID' };
+}
+
+function defaultMatrix2(profile, matrixOptions) {
+  const options = options.map(({ value }) => value);
+
+  if (profile.value == 'SBS')
+    return options.includes('96')
+      ? { label: '96', value: '96' }
+      : matrixOptions[0];
+
+  if (profile.value == 'DBS')
+    return options.includes('78')
+      ? { label: '78', value: '78' }
+      : matrixOptions[0];
+
+  if (profile.value == 'ID')
+    return options.includes('83')
+      ? { label: '83', value: '83' }
+      : matrixOptions[0];
+}
+
+function defaultFilter2(filterOptions) {
+  const options = options.map(({ value }) => value);
+  return options.includes('NA')
+    ? { label: 'NA', value: 'NA' }
+    : filterOptions[0];
+}
+
 module.exports = {
   defaultProfile,
   defaultMatrix,
   defaultFilter,
+  defaultProfile2,
+  defaultMatrix2,
+  defaultFilter2,
 };
