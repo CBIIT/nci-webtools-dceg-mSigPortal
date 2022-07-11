4f3b4a3bcbfe526ea199b1b66b1767f024052137
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-07-11 11:24:02 -0400

    sort exposure data by cancer and sample

--- client/src/components/controls/plotly/tmb/tmb.js
+++ client/src/components/controls/plotly/tmb/tmb.js
@@ -1,107 +1,72 @@
-export default function TMB(data, study) {
-  console.log("data:");
-  console.log(data);
-  const genome = { PCAWG: "GRCh37", TCGA: "GRCh37" };
+import { groupBy } from 'lodash';
+
+export default function TMB(data, study = 'PCAWG') {
+  // Calculate the number of mutations per megabase for each study
+  const genome = { PCAWG: 'GRCh37', TCGA: 'GRCh37' };
   const genomeSize = { GRCh37: 3101976562 / Math.pow(10, 6) };
+  const burden = (exposure) => Math.log10(exposure / genomeSize[genome[study]]);
 
   function average(arr) {
     const sum = arr.reduce((a, b) => a + b, 0);
     return sum / arr.length || 0;
   }
-  const groupByCancer = data.reduce((groups, e) => {
-    const cancerName = e.cancer;
-    groups[cancerName] = groups[cancerName] ? [...groups[cancerName], e] : [e];
-    return groups;
-  }, {});
+  const groupByCancer = groupBy(data, 'cancer');
 
-  console.log("group by cancer:");
-  console.log(groupByCancer);
+  // calculate burden per cancer/sample
+  const cancerBurden = Object.entries(groupByCancer)
+    .map(([cancer, values]) => {
+      const groupBySample = groupBy(values, 'sample');
+      const tmbs = Object.entries(groupBySample).map(([_, e]) =>
+        burden(e.reduce((sum, e) => e.exposure + sum, 0))
+      );
+      return { cancer, tmbs };
+    })
+    .sort((a, b) => (a.cancer > b.cancer ? 1 : b.cancer > a.cancer ? -1 : 0));
 
-  const groupBySamples = Object.entries(groupByCancer).map(
-    ([cancerName, cancerArray]) => {
-      const sampleGroups = cancerArray.reduce((sampleGroup, e) => {
-        const sampleName = e.cancer;
-        sampleGroup[sampleName] = sampleGroup[sampleName]
-          ? [...sampleGroup[sampleName], e]
-          : [e];
-        return sampleGroup;
-      }, {});
-
-      return { cancer: cancerName, samples: sampleGroups };
-    },
-    {}
-  );
-
-  console.log("groupBySamples");
-  console.log(groupBySamples);
-  const burdenQuotient = genomeSize[genome[study]];
-
-  console.log("burdenQuotient");
-  console.log(burdenQuotient);
-  const cancerBurden = groupBySamples.map(({ cancer, samples }) => {
-    const sampleBurden = Object.entries(samples).map(
-      ([cancerName, sampleArray]) => ({
-        sample: cancerName,
-        burden: Math.log10(
-          sampleArray.reduce((sum, e) => e.exposure + sum, 0) / burdenQuotient
-        ),
-      })
-    );
-    sampleBurden.sort((a, b) => a.burden - b.burden);
-
-    return { cancer, sampleBurden };
-  });
-
-  const flatSorted = Object.values(cancerBurden).flat();
-
-  console.log("Cancer Budern:");
   console.log(cancerBurden);
-
-  console.log("flatSorted:");
-  console.log(flatSorted);
 
   const traces = cancerBurden.map((element, index, array) => ({
     element: element,
     index: index,
     array: array,
     name: `${element.cancer}`,
-    type: "scatter",
-    marker: { symbol: "circle-open", size: 4, color: "black" },
-    mode: "markers",
-    y: element.sampleBurden.map((e) => e.burden),
-    average: average(element.sampleBurden.map((e) => e.burden)),
-    hovertemplate: "Burden: %{y}<br>",
-    // x: element.sampleBurden.map(
+    type: 'scatter',
+    marker: { symbol: 'circle-open', size: 4, color: 'black' },
+    mode: 'markers',
+    y: element.tmbs.map((e) => e),
+    average: average(element.tmbs.map((e) => e)),
+    hovertemplate: 'Burden: %{y}<br>',
+    // x: element.tmbs.map(
     //   (e, i) =>
     //     array
     //       .slice(0, index)
-    //       .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0) +
+    //       .reduce((x0, curr) => x0 + curr.tmbs.length, 0) +
     //     i +
     //     0.5
     // ),
     x:
-      array.lenght > 1
-        ? element.sampleBurden.map(
-            (e, i) => index + 0.7 + (0.3 / element.sampleBurden.length) * i
+      array.length > 1
+        ? element.tmbs.map(
+            (e, i) => index + 0.7 + (0.3 / element.tmbs.length) * i
           )
-        : element.sampleBurden.map(
-            (e, i) => index + 0.07 + (0.8 / element.sampleBurden.length) * i
+        : element.tmbs.map(
+            (e, i) => index + 0.07 + (0.8 / element.tmbs.length) * i
           ),
     showlegend: false,
   }));
-  console.log("traces:--");
+  console.log('traces:--');
   console.log(traces);
 
   const topLabel = cancerBurden.map((element, index, array) => ({
-    xref: "x",
-    yref: "paper",
-    xanchor: "bottom",
-    yanchor: "bottom",
+    xref: 'x',
+    yref: 'paper',
+    xanchor: 'bottom',
+    yanchor: 'bottom',
     // x:
     //   array
     //     .slice(0, index)
-    //     .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0) +
-    //   (element.sampleBurden.length - 1) * 0.5,
+    //     .reduce((x0, curr) => x0 + curr.tmbs.length, 0) +
+    //   (element.tmbs.length - 1) * 0.5,
     //x: array.length * 0.5,
     x: array.length > 1 ? index : (index + index + 1) * 0.5,
     y: 1.0,
@@ -110,61 +75,61 @@ export default function TMB(data, study) {
     // font: {
     //   size: 12,
     // },
-    align: "right",
+    align: 'right',
     textangle: 45,
   }));
-  console.log("top label:--");
+  console.log('top label:--');
   console.log(topLabel);
 
   const bottoLabel1 = cancerBurden.map((element, index, array) => ({
-    xref: "x",
-    yref: "paper",
-    xanchor: "bottom",
-    yanchor: "bottom",
+    xref: 'x',
+    yref: 'paper',
+    xanchor: 'bottom',
+    yanchor: 'bottom',
     x: (index + index + 1) * 0.5,
     y: -0.12,
-    text: `${element.sampleBurden.length}<br> - <br>`,
+    text: `${element.tmbs.length}<br> - <br>`,
     showarrow: false,
     font: {
       size: 12,
-      color: "blue",
+      color: 'blue',
     },
-    align: "center",
+    align: 'center',
   }));
 
   const bottoLabel2 = cancerBurden.map((element, index, array) => ({
-    xref: "x",
-    yref: "paper",
-    xanchor: "bottom",
-    yanchor: "bottom",
+    xref: 'x',
+    yref: 'paper',
+    xanchor: 'bottom',
+    yanchor: 'bottom',
     x: (index + index + 1) * 0.5,
     y: -0.17,
-    text: `${element.sampleBurden.length}`,
+    text: `${element.tmbs.length}`,
     showarrow: false,
     font: {
       size: 12,
-      color: "green",
+      color: 'green',
     },
-    align: "center",
+    align: 'center',
   }));
 
   const shapes = cancerBurden.map((element, index, array) => ({
-    type: "rect",
-    xref: "x",
-    yref: "paper",
+    type: 'rect',
+    xref: 'x',
+    yref: 'paper',
 
     // x0: array
     //   .slice(0, index)
-    //   .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0),
+    //   .reduce((x0, curr) => x0 + curr.tmbs.length, 0),
 
     // x1: array
     //   .slice(0, index + 1)
-    //   .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0),
+    //   .reduce((x0, curr) => x0 + curr.tmbs.length, 0),
     x0: index,
     x1: index + 1,
     y0: 0,
     y1: 1,
-    fillcolor: index % 2 === 0 ? "gray" : "#F8F8F8",
+    fillcolor: index % 2 === 0 ? 'gray' : '#F8F8F8',
     line: {
       width: 0,
     },
@@ -172,29 +137,29 @@ export default function TMB(data, study) {
   }));
 
   const lines = cancerBurden.map((element, index, array) => ({
-    type: "line",
-    xref: "x",
-    yref: "y",
+    type: 'line',
+    xref: 'x',
+    yref: 'y',
 
     // x0: array
     //   .slice(0, index)
-    //   .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0),
+    //   .reduce((x0, curr) => x0 + curr.tmbs.length, 0),
 
     // x1: array
     //   .slice(0, index + 1)
-    //   .reduce((x0, curr) => x0 + curr.sampleBurden.length, 0),
+    //   .reduce((x0, curr) => x0 + curr.tmbs.length, 0),
     x0: index + 0.1,
     x1: index + 0.9,
 
-    y0: average(element.sampleBurden.map((e) => e.burden)),
-    y1: average(element.sampleBurden.map((e) => e.burden)),
+    y0: average(element.tmbs.map((e) => e)),
+    y1: average(element.tmbs.map((e) => e)),
     line: {
       width: 1,
-      color: "red",
+      color: 'red',
     },
   }));
 
-  console.log("shapes:--");
+  console.log('shapes:--');
   console.log(shapes);
 
   const layout = {
@@ -207,20 +172,20 @@ export default function TMB(data, study) {
       tickfont: {
         size: 10,
       },
-      linecolor: "black",
+      linecolor: 'black',
       linewidth: 2,
       mirror: true,
-      tickmode: "array",
+      tickmode: 'array',
       showgrid: false,
       //tickvals: flatSorted.map((_, i) => i),
       //ticktext: flatSorted.map((_, i) => i),
     },
     yaxis: {
-      title: "Number of Mutations per Megabase<br>(log10)",
+      title: 'Number of Mutations per Megabase<br>(log10)',
       autorange: true,
       zeroline: false,
       //showline: true,
-      linecolor: "black",
+      linecolor: 'black',
       linewidth: 2,
       mirror: true,
       automargin: true,
@@ -229,7 +194,7 @@ export default function TMB(data, study) {
     shapes: [...shapes, ...lines],
     annotations: [...topLabel, ...bottoLabel1, ...bottoLabel2],
   };
-  console.log("layout:");
+  console.log('layout:');
   console.log(layout);
 
   var config = {
