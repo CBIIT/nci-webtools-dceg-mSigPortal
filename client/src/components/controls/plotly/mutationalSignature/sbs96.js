56dd0a1a4b041f6f8832deb2f56f6edae6477aeb
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-07-15 15:40:16 -0400

    refactor sbs96 for mutational profiles api

--- client/src/components/controls/plotly/mutationalSignature/sbs96.js
+++ client/src/components/controls/plotly/mutationalSignature/sbs96.js
@@ -7,97 +7,60 @@ export default function SBS96(data, sample) {
     'T>C': '#A1CE63',
     'T>G': '#EBC6C4',
   };
-  //console.log("data--:");
-  //console.log(data);
+
   const numberWithCommas = (x) =>
     x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 
-  const totalMutations = data.reduce((a, e) => a + parseInt(e.mutations), 0);
-  const maxVal = Math.max(...data.map((o) => o.mutations));
-  // console.log("maxVal--:");
-  // console.log(maxVal);
-
-  // group data by dominant mutation
-  const groupByMutation = data.reduce((groups, e, i) => {
-    const mutationRegex = /\[(.*)\]/;
-    const mutation = e.mutationType.match(mutationRegex)[1];
-    const signature = {
-      mutationType: e.mutationType,
-      contribution: e.mutations,
-    };
-    groups[mutation] = groups[mutation]
-      ? [...groups[mutation], signature]
-      : [signature];
-    return groups;
-  }, {});
-  const flatSorted = Object.values(groupByMutation).flat();
-
-  // console.log("groupByMutation:");
-  // console.log(groupByMutation);
-  //console.log("FlatSorted");
-  //console.log(flatSorted);
-  const traces = Object.entries(groupByMutation).map(
-    ([mutation, signatures], groupIndex, array) => ({
-      name: mutation,
-      type: 'bar',
-      marker: { color: colors[mutation] },
-      //   x: signatures.map((e) => e.mutationType),
-      //x: signatures.map((e, i) => groupIndex * signatures.length + i),
-      x: signatures.map(
-        (e, i) =>
-          array
-            .slice(0, groupIndex)
-            .reduce((x0, [_, sigs]) => x0 + sigs.length, 0) + i
-      ),
-      y: signatures.map((e) => e.contribution),
-      hoverinfo: 'x+y',
-      showlegend: false,
-    })
+  const totalMutations = data.reduce(
+    (total, base) =>
+      total +
+      base.mutationTypes.reduce((baseSum, e) => baseSum + e.mutations, 0),
+    0
   );
-  // console.log("traces:");
-  // console.log(traces);
+  const maxMutation = Math.max(
+    ...data.map((base) => base.mutationTypes.map((e) => e.mutations)).flat()
+  );
 
-  const annotations = Object.entries(groupByMutation).map(
-    ([mutation, signatures], groupIndex, array) => ({
-      xref: 'x',
-      yref: 'paper',
-      xanchor: 'bottom',
-      yanchor: 'bottom',
-      x:
+  const mutationTypeNames = data
+    .map((group) =>
+      group.mutationTypes.map((e) => ({
+        base: group.base,
+        mutationType: e.mutationType,
+      }))
+    )
+    .flat();
+
+  const traces = data.map((group, groupIndex, array) => ({
+    name: group.base,
+    type: 'bar',
+    marker: { color: colors[group.base] },
+    x: [...group.mutationTypes.keys()].map(
+      (e) =>
+        e +
         array
           .slice(0, groupIndex)
-          .reduce((x0, [_, sigs]) => x0 + sigs.length, 0) +
-        (signatures.length - 1) * 0.5,
-      y: 1.04,
-      text: `<b>${mutation}</b>`,
-      showarrow: false,
-      font: {
-        size: 18,
-      },
-      align: 'center',
-    })
-  );
+          .reduce((lastIndex, b) => lastIndex + b.mutationTypes.length, 0)
+    ),
+    y: group.mutationTypes.map((e) => e.mutations),
+    hoverinfo: 'x+y',
+    showlegend: false,
+  }));
 
-  const xannotations = flatSorted.map((num, index) => ({
+  const baseAnnotation = data.map((group, groupIndex, array) => ({
     xref: 'x',
     yref: 'paper',
     xanchor: 'bottom',
     yanchor: 'bottom',
-    x: index,
-    y: -0.1,
-    text: num.mutationType.replace(
-      /\[(.*)\]/,
-      num.mutationType.substring(2, 3)
-    ),
+    x:
+      array
+        .slice(0, groupIndex)
+        .reduce((lastIndex, b) => lastIndex + b.mutationTypes.length, 0) +
+      (group.mutationTypes.length - 1) * 0.5,
+    y: 1.04,
+    text: `<b>${group.base}</b>`,
     showarrow: false,
-    font: {
-      size: 10,
-      color: colors[num.mutationType.substring(2, 5)],
-    },
+    font: { size: 18 },
     align: 'center',
-    num: num,
-    index: index,
-    textangle: -90,
   }));
 
   const sampleAnnotation = {
@@ -116,28 +79,30 @@ export default function SBS96(data, sample) {
     align: 'center',
   };
 
-  const shapes = Object.entries(groupByMutation).map(
-    ([mutation, _], groupIndex, array) => ({
-      type: 'rect',
-      xref: 'x',
-      yref: 'paper',
-      x0: array
-        .slice(0, groupIndex)
-        .reduce((x0, [_, sigs]) => x0 + sigs.length, -0.4),
-      x1: array
-        .slice(0, groupIndex + 1)
-        .reduce((x0, [_, sigs]) => x0 + sigs.length, -0.6),
-      y0: 1.05,
-      y1: 1.01,
-      fillcolor: colors[mutation],
-      line: {
-        width: 0,
-      },
-      mutation: mutation,
-    })
-  );
-  // console.log("shapes");
-  // console.log(shapes);
+  const shapes = data.map((group, groupIndex, array) => ({
+    type: 'rect',
+    xref: 'x',
+    yref: 'paper',
+    x0: array
+      .slice(0, groupIndex)
+      .reduce((lastIndex, e) => lastIndex + e.mutationTypes.length, -0.35),
+    x1: array
+      .slice(0, groupIndex + 1)
+      .reduce((lastIndex, e) => lastIndex + e.mutationTypes.length, -0.65),
+    y0: 1.05,
+    y1: 1.01,
+    fillcolor: colors[group.base],
+    line: {
+      width: 0,
+    },
+  }));
+
+  function formatTickLabel(base, mutationType) {
+    const color = colors[base];
+    const regex = /^(.)\[(.).{2}\](.)$/;
+    const match = mutationType.match(regex);
+    return `${match[1]}<span style="color:${color}">${match[2]}</span>${match[3]}`;
+  }
 
   const layout = {
     hoverlabel: { bgcolor: '#FFF' },
@@ -147,27 +112,22 @@ export default function SBS96(data, sample) {
     autosize: true,
 
     xaxis: {
-      showticklabels: false,
       showline: true,
       tickangle: -90,
-      tickfont: {
-        size: 10,
-      },
+      tickfont: { size: 11 },
       tickmode: 'array',
-      tickvals: flatSorted.map((_, i) => i),
-      // ticktext: flatSorted.map((e) =>
-      //   e.mutationType.replace(/\[(.*)\]/, e.mutationType.substring(2, 3))
-      // ),
-      ticktext: flatSorted.map((e) => e.mutationType),
+      tickvals: mutationTypeNames.map((_, i) => i),
+      ticktext: mutationTypeNames.map((e) =>
+        formatTickLabel(e.base, e.mutationType)
+      ),
       linecolor: '#E0E0E0',
       linewidth: 1,
       mirror: 'all',
-      tickformat: '~s',
     },
     yaxis: {
       title: 'Number of Single Base Substitutions',
       autorange: false,
-      range: [0, maxVal + maxVal * 0.2],
+      range: [0, maxMutation + maxMutation * 0.2],
       ticks: 'inside',
       linecolor: '#E0E0E0',
       linewidth: 1,
@@ -177,11 +137,8 @@ export default function SBS96(data, sample) {
     },
 
     shapes: shapes,
-    annotations: [...annotations, sampleAnnotation, ...xannotations],
+    annotations: [...baseAnnotation, sampleAnnotation],
   };
-  // console.log("layout");
-  // console.log(layout);
 
-  //var config = { responsive: true };
   return { traces, layout };
 }
--- client/src/components/pages/visualization/mutationalProfiles/apiSlice.js
+++ client/src/components/pages/visualization/mutationalProfiles/apiSlice.js
@@ -3,7 +3,7 @@ import { apiSlice } from '../../../../services/apiSlice';
 export const mutationalProfilesApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     mutationalProfiles: builder.query({
-      query: (params) => ({ url: 'seqmatrix', params }),
+      query: (params) => ({ url: 'mutationalProfiles', params }),
     }),
   }),
 });
