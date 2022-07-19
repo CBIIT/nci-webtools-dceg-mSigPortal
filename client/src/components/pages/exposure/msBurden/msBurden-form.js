429c2c48d721ed6038cb855c12f6c295caf89daa
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-07-19 11:23:02 -0400

    fix msBurden form

--- client/src/components/pages/exposure/msBurden/msBurden-form.js
+++ client/src/components/pages/exposure/msBurden/msBurden-form.js
@@ -1,26 +1,36 @@
 import React, { useEffect } from 'react';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-// { LoadingOverlay } from '../../controls/loading-overlay/loading-overlay';
 import Select from '../../../controls/select/selectForm';
 import Description from '../../../controls/description/description';
 import { useSelector, useDispatch } from 'react-redux';
 import { actions as visualizationActions } from '../../../../services/store/visualization';
-//import CustomSelect from '../../controls/select/select-old';
-
+import {
+  defaultProfile2,
+  defaultMatrix2,
+  defaultFilter2,
+} from '../../../../services/utils';
 import { NavHashLink } from 'react-router-hash-link';
 
 const actions = { ...visualizationActions };
 
-export default function MsBurden() {
+export default function TreeLeafForm() {
   const dispatch = useDispatch();
   const store = useSelector((state) => state.exposure);
 
-  const { signatureName } = store.main;
-  console.log(signatureName);
+  const { signatureNames } = store.main;
 
-  //   const mergeMsBurden = (state) =>
-  //     dispatch(actions.mergeExposure({ msBurden: state }));
+  console.log(signatureNames);
+  const { control, setValue, watch } = useForm();
+
+  const signatureNameOptions = signatureNames.length
+    ? [...new Set(signatureNames.map((d) => d))]
+        .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
+        .map((e) => ({
+          label: e,
+          value: e,
+        }))
+    : [];
 
   return (
     <div>
@@ -29,23 +39,15 @@ export default function MsBurden() {
         <Row>
           <Col lg="auto">
             <Select
-              name="signatureName"
+              name="signatureNames"
               label="Signature Name"
-              value={signatureName}
-              //   options={
-              //     source == 'public' ? signatureNameOptions : userNameOptions
-              //   }
-              //onChange={(name) => mergeMsBurden({ signatureName: name })}
+              value={signatureNames}
+              options={signatureNameOptions}
+              control={control}
             />
           </Col>
-          <Col lg="auto" className="d-flex">
-            <Button
-              className="mt-auto mb-2"
-              variant="primary"
-              //onClick={calculateBurden}
-            >
-              Recalculate
-            </Button>
+          <Col>
+            <Button>Recalculate</Button>
           </Col>
         </Row>
       </Form>
