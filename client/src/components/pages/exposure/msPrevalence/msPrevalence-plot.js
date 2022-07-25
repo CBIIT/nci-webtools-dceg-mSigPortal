2f5c99cbf5d473c1cc805f7c2c1f10d49952b4b0
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2022-07-25 14:18:54 -0400

    add msPrevalence tab

---
+++ client/src/components/pages/exposure/msPrevalence/msPrevalence-plot.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { cloneDeep } from 'lodash';
+import { useSelector } from 'react-redux';
+import { useLazyMsPrevelenceQuery } from './apiSlice';
+import { LoadingOverlay } from '../../../controls/loading-overlay/loading-overlay';
+
+import './plot.scss';
+export default function MutProfilePlot() {
+  const publicForm = useSelector((state) => state.exposure.publicForm);
+  const [params, setParams] = useState('');
+  const { data, error, isFetching } = useLazyMsPrevelenceQuery(params, {
+    skip: !params,
+  });
+
+  useEffect(() => {
+    const { study, strategy, signatureSetName, cancer } = publicForm;
+    if (study) {
+      setParams({
+        study: study.value,
+        strategy: strategy.value,
+        signatureSetName: signatureSetName.value,
+        cancer: cancer.value,
+      });
+    }
+  }, [publicForm]);
+
+  console.log(data);
+  return (
+    <>
+      <LoadingOverlay active={isFetching} />
+      {data && (
+        <Container fluid style={{ minHeight: '500px' }} className="mb-3">
+          <Row>
+            <Col>
+              <Plot
+                className="w-100"
+                data={cloneDeep(data.traces)}
+                layout={cloneDeep(data.layout)}
+                config={cloneDeep(data.config)}
+                useResizeHandler
+              />
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {error && <p>An error has occured</p>}
+    </>
+  );
+}
