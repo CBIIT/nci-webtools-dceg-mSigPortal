f58a421153aff6765d1ec581de201783bfd4ce30
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-07-29 16:40:56 -0400

    save seqmatrix options instead of entire study

--- client/src/components/pages/visualization/publicForm/apiSlice.js
+++ client/src/components/pages/visualization/publicForm/apiSlice.js
@@ -4,7 +4,7 @@ export const publicFormApiSlice = visualizationApiSlice.injectEndpoints({
   endpoints: (builder) => ({
     publicMatrix: builder.mutation({
       query: (params) => ({
-        url: 'seqmatrix',
+        url: 'seqmatrixOptions',
         params,
       }),
     }),
