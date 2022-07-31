8d577086e95b7528ecac3cf3927a725018469506
Brian Park <park-brian@users.noreply.github.com>
2022-07-31 19:02:05 -0400

    add exportJsonAsCsv

---
+++ utils/exportJsonAsCsv.R
@@ -0,0 +1,23 @@
+library(jsonlite)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  stop("Usage: Rscript exportJsonAsCsv.R inputFile.json columnName=columnValue outputFile.csv")
+}
+
+inputFile <- args[1]
+additionalColumn <- unlist(strsplit(args[2], '='))
+outputFile <- args[3]
+
+loadJson <- function(filepath, additionalColumn) {
+  dataset <- read_json(filepath, simplifyVector = T)
+  columnName = additionalColumn[1]
+  columnValue = additionalColumn[2]
+  dataset[[columnName]] <- columnValue
+  dataset
+}
+
+dataset <- loadJson(inputFile)
+write.csv(dataset, file = outputFile, na="", row.names = F)
+
