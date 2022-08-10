23615ca311d6a5df994f8b52a8f19c61303cc7ef
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2022-08-10 11:40:44 -0400

    move code from apislice to scatter plot

--- client/src/components/controls/plotly/mutationalPattern/mutationalPatternScatter.js
+++ client/src/components/controls/plotly/mutationalPattern/mutationalPatternScatter.js
@@ -1,12 +1,122 @@
-import { text } from 'd3';
+import { groupBy } from 'lodash';
 
-export default function mutationalPatternScatter(
-  data,
-  type,
-  pattern1,
-  pattern2
-) {
-  const maxTotal = Math.max(...data.map((o) => o.total));
+export default function mutationalPatternScatter(rawdata, arg) {
+  const { pattern } = arg;
+  const type =
+    pattern.substring(1, 2) + pattern.substring(3, 4) + pattern.substring(5, 6);
+  const subtype1 = pattern.substring(0, 1);
+  const subtype2 = pattern.substring(2, 3);
+  const pattern1 = type + ' context';
+  const pattern2 = pattern + ' other context';
+
+  const tmpdata0 = Object.values(
+    groupBy(rawdata, (e) => `${e.study}_${e.sample}`)
+  ).map((samples) => {
+    return {
+      study: `${samples[0].study}`,
+      sample: `${samples[0].sample}`,
+      type: type,
+      total: samples.reduce((acc, e) => acc + e.mutations, 0),
+    };
+  });
+  const mutationTypeFilter = rawdata.filter(
+    (e) => e.mutationType.substring(2, 5) === type
+  );
+  const groupByStudySampleType = groupBy(
+    mutationTypeFilter,
+    (e) => `${e.study}_${e.sample}`
+  );
+  const tmpdata1 = Object.values(groupByStudySampleType).map((samples) => {
+    return {
+      study: `${samples[0].study}`,
+      sample: `${samples[0].sample}`,
+      n0: samples.reduce((acc, e) => acc + e.mutations, 0),
+    };
+  });
+
+  function iupac(base) {
+    let result = [];
+    if (base === 'T' || base === 'U') {
+      result = ['T'];
+    } else if (base === 'M') {
+      result = ['A', 'C'];
+    } else if (base === 'R') {
+      result = ['A', 'G'];
+    } else if (base === 'W') {
+      result = ['A', 'T'];
+    } else if (base === 'S') {
+      result = ['C', 'G'];
+    } else if (base === 'Y') {
+      result = ['C', 'T'];
+    } else if (base === 'K') {
+      result = ['G', 'T'];
+    } else if (base === 'V') {
+      result = ['A', 'C', 'G'];
+    } else if (base === 'H') {
+      result = ['A', 'C', 'T'];
+    } else if (base === 'D') {
+      result = ['A', 'G', 'T'];
+    } else if (base === 'B') {
+      result = ['C', 'G', 'T'];
+    } else if (base === 'N') {
+      result = ['G', 'A', 'T', 'C'];
+    } else {
+      result = [base];
+    }
+    return result;
+  }
+  const mutationTypeSubTypesFilter = rawdata.filter(
+    (e) =>
+      e.mutationType.substring(2, 5) === type &&
+      iupac(subtype1).includes(e.mutationType.substring(0, 1)) &&
+      iupac(subtype2).includes(e.mutationType.substring(6, 7))
+  );
+
+  const groupByStudySampleTypeFilter = groupBy(
+    mutationTypeSubTypesFilter,
+    (e) => `${e.study}_${e.sample}`
+  );
+
+  const tmpdata2 = Object.values(groupByStudySampleTypeFilter).map(
+    (samples) => {
+      return {
+        study: `${samples[0].study}`,
+        sample: `${samples[0].sample}`,
+        cancer: `${samples[0].cancer}`,
+        type: type,
+        n1: samples.reduce((acc, e) => acc + e.mutations, 0),
+      };
+    }
+  );
+  const merge = (a1, a2) =>
+    a1.map((itm) => ({
+      ...a2.find(
+        (item) => item.sample === itm.sample && item.study === itm.study && item
+      ),
+      ...itm,
+    }));
+
+  const result0 = merge(tmpdata1, tmpdata0);
+  const result1 = merge(tmpdata2, result0);
+
+  const result = result1
+    .map((e) => {
+      let n2 = e.n0 - e.n1;
+
+      return {
+        study: e.study,
+        sample: e.sample,
+        cancer: e.cancer,
+        total: e.total,
+        type: pattern,
+        n0: e.n0,
+        n1: e.n1 / e.total,
+        n2: n2 / e.total,
+      };
+    })
+    .filter((e) => e.total > 200);
+
+  const maxTotal = Math.max(...result.map((o) => o.total));
   console.log(maxTotal);
   console.log(maxTotal.toExponential());
   function format_output(output) {
@@ -19,11 +129,11 @@ export default function mutationalPatternScatter(
   }
   const maxMutationFilter = format_output(maxTotal);
   console.log(maxMutationFilter);
-  const data1 = data.filter((o) => o.total < maxMutationFilter / 100);
-  const data2 = data.filter(
+  const data1 = result.filter((o) => o.total < maxMutationFilter / 100);
+  const data2 = result.filter(
     (o) => o.total < maxMutationFilter / 10 && o.total > maxMutationFilter / 100
   );
-  const data3 = data.filter((o) => o.total > maxMutationFilter / 10);
+  const data3 = result.filter((o) => o.total > maxMutationFilter / 10);
 
   var trace1 = {
     name: (maxMutationFilter / 100).toLocaleString(undefined),
@@ -89,10 +199,10 @@ export default function mutationalPatternScatter(
     },
   };
   var trace4 = {
-    name: data[0].study + '@' + data[0].cancer,
-    x: data.map((e) => e.n1),
-    y: data.map((e) => e.n2),
-    customdata: data.map((e) => ({ total: e.total })),
+    name: result[0].study + '@' + result[0].cancer,
+    x: result.map((e) => e.n1),
+    y: result.map((e) => e.n2),
+    customdata: result.map((e) => ({ total: e.total })),
     mode: 'markers',
     type: 'scatter',
     opacity: 1,
@@ -102,7 +212,7 @@ export default function mutationalPatternScatter(
         color: 'black',
         width: 1,
       },
-      size: data.map((e, i, a) =>
+      size: result.map((e, i, a) =>
         e.total < maxMutationFilter / 100
           ? 5
           : e.total < maxMutationFilter / 10
