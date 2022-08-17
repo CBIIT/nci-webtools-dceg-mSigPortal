e011eda28822b30a1b9b3c98522b74540210b844
Brian Park <park-brian@users.noreply.github.com>
2022-08-17 14:14:41 -0400

    fix formatting

--- utils/exportJsonAsCsv.R
+++ utils/exportJsonAsCsv.R
@@ -1,3 +1,4 @@
+library(dplyr, warn.conflicts = FALSE)
 library(jsonlite)
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -11,13 +12,15 @@ additionalColumn <- unlist(strsplit(args[2], '='))
 outputFile <- args[3]
 
 loadJson <- function(filepath, additionalColumn) {
-  dataset <- read_json(filepath, simplifyVector = T)
+  dataset <- read_json(filepath, simplifyVector = T) %>% 
+    rowwise() %>% 
+    mutate(across(where(~ class(.x) == "list"), ~ paste(., collapse = " ")))
   columnName = additionalColumn[1]
   columnValue = additionalColumn[2]
   dataset[[columnName]] <- columnValue
   dataset
 }
 
-dataset <- loadJson(inputFile)
+dataset <- loadJson(inputFile, additionalColumn)
 write.csv(dataset, file = outputFile, na="", row.names = F)
 
