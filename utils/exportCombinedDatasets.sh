f44b65be71455dcb03c758720fa243e3b6059757
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-08-19 17:57:04 -0400

    update rdata conversion utils

--- utils/exportCombinedDatasets.sh
+++ utils/exportCombinedDatasets.sh
@@ -6,3 +6,5 @@ Rscript combineDatasets.R 'Exposure/exposure_refdata.RData' combineExposureFiles
 Rscript combineDatasets.R 'Seqmatrix/*_*_seqmatrix_refdata.RData' combineSeqmatrixFiles Seqmatrix/data.csv
 Rscript combineDatasets.R 'Signature/signature_refsets.RData' combineSignatureFiles Signature/data.csv
 Rscript combineDatasets.R 'Others/content_data_all.RData' combinePatternFiles Others/pattern.csv
+Rscript combineDatasets.R 'Etiology/aetiology_exposure.RData' combineEtiology Etiology/etiology.csv
+Rscript combineDatasets.R 'Etiology/aetiology_organ_specific_signature.RData' combineEtiologyOrgan Etiology/etiologyOrgan.csv
