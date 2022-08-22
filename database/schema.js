419e0c146d4b2da0da7ed5d51828bc5f6ea7bc71
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-08-22 15:16:14 -0400

    fix profiler summary math

--- database/schema.js
+++ database/schema.js
@@ -176,7 +176,7 @@ export const schema = [
       const summaryColumns = [
         ...columns.slice(0, -3),
         connection.raw(`string_agg(cast(matrix as text), '/' order by matrix asc) as matrix`),
-        connection.raw(`log10(sum("totalMutations")) as "logTotalMutations"`),
+        connection.raw(`log10("totalMutations") as "logTotalMutations"`),
         connection.raw(`avg("totalMutations") as "meanTotalMutations"`),
       ];
       const totalCountQuery = connection
