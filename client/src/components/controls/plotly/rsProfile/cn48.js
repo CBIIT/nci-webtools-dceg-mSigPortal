fa8b425b7368df5db9c500ae3f50a46304c9eb2e
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2022-09-13 16:00:47 -0400

    making 2nd group for CN

--- client/src/components/controls/plotly/rsProfile/cn48.js
+++ client/src/components/controls/plotly/rsProfile/cn48.js
@@ -74,63 +74,25 @@ export default function CN48(rawData, sample) {
   );
   console.log(groupByClusterData);
 
-  const groupByIndelhd = hd.reduce((acc, e, i) => {
-    const indel = e.mutationType.substring(0, 13);
+  const groupbyfirst2 = rawData.reduce((acc, e, i) => {
+    const names = e.mutationType.split(':');
 
-    acc[indel] = acc[indel] ? [...acc[indel], e] : [e];
+    acc[names[0] + ':' + names[1]] = acc[names[0] + ':' + names[1]]
+      ? [...acc[names[0] + ':' + names[1]], e]
+      : [e];
     return acc;
   }, {});
-  console.log(groupByIndelhd);
-  const groupByIndelLOH = LOH.reduce((acc, e, i) => {
-    const indel = e.mutationType.substring(0, 17);
+  console.log(groupbyfirst2);
 
-    acc[indel] = acc[indel] ? [...acc[indel], e] : [e];
-    return acc;
-  }, {});
-
-  const groupByIndelHet = het.reduce((acc, e, i) => {
-    const indel = e.mutationType.substring(0, 17);
-
-    acc[indel] = acc[indel] ? [...acc[indel], e] : [e];
-    return acc;
-  }, {});
-  console.log(groupByIndelLOH);
-  const groupByIndelhdGroup = Object.entries(groupByIndelhd).map(
-    ([indel, data]) => ({
-      indel,
-      data,
-    })
-  );
-
-  const groupByIndelLOHGroup = Object.entries(groupByIndelLOH).map(
-    ([indel, data]) => ({
-      indel,
-      data,
-    })
-  );
-  const groupByIndelHetGroup = Object.entries(groupByIndelHet).map(
-    ([indel, data]) => ({
-      indel,
-      data,
-    })
-  );
-
-  const data = [
-    ...groupByIndelhdGroup,
-    ...groupByIndelLOHGroup,
-    ...groupByIndelHetGroup,
-  ];
-  console.log(data);
-
-  const dataD = data.map((indel) => indel.data.map((e) => e)).flat();
-  console.log(dataD);
-  const mutationTypeNames = dataD
-    .map((group) =>
-      group.data.map((e, i) => ({
-        mutationType: e.mutationType.split(':')[2],
-      }))
-    )
+  const dataD = groupByClusterData
+    .map((indel) => indel.data.map((e) => e))
     .flat();
+  console.log(dataD);
+  const mutationTypeNames = dataD.map((group, i) => ({
+    mutationType: group.mutationType.split(':')[2],
+    index: i,
+  }));
+
   console.log(mutationTypeNames);
 
   const traces = dataD.map((group, groupIndex, array) => ({
@@ -160,151 +122,30 @@ export default function CN48(rawData, sample) {
     showlegend: false,
   }));
   console.log(traces);
-  const traces1 = data.map((group, groupIndex, array) => ({
-    group: group,
-    name: group.indel,
-    type: 'bar',
-    marker: { color: colors[group.mutationType] },
-    x: [...group.data.keys()].map(
-      (e) =>
-        e +
-        array
-          .slice(0, groupIndex)
-          .reduce((lastIndex, b) => lastIndex + b.data.length, 0)
-    ),
-    y: group.data.map((e) => e.contribution),
-    showlegend: false,
-  }));
-  const topShapes = data.map((group, groupIndex, array) => ({
+
+  const topShapes = dataD.map((group, groupIndex, array) => ({
     group: group,
     name: group.indel,
     type: 'rect',
     xref: 'x',
     yref: 'paper',
-    marker: {
-      color:
-        colors[
-          group.indel.substring(group.indel.length - 3, group.indel.length)
-        ],
-    },
-    x0: array
-      .slice(0, groupIndex)
-      .reduce((lastIndex, e) => lastIndex + e.data.length, -0.4),
-    x1: array
-      .slice(0, groupIndex + 1)
-      .reduce((lastIndex, e) => lastIndex + e.data.length, -0.6),
-    y0: 1.07,
-    y1: 1.01,
-    fillcolor:
-      colors[group.indel.substring(group.indel.length - 3, group.indel.length)],
-    line: {
-      width: 0,
-    },
-    showlegend: false,
+
+    // x0: array
+    //   .slice(0, groupIndex)
+    //   .reduce((lastIndex, e) => lastIndex + e.data.length, -0.4),
+    // x1: array
+    //   .slice(0, groupIndex + 1)
+    //   .reduce((lastIndex, e) => lastIndex + e.data.length, -0.6),
+    // y0: 1.07,
+    // y1: 1.01,
+    // fillcolor: colors[group.indel.split(':')[0]],
+    // line: {
+    //   width: 0,
+    // },
+    // showlegend: false,
   }));
   console.log(topShapes);
 
-  const topShapeAnnitations = data.map((group, groupIndex, array) => ({
-    xref: 'x',
-    yref: 'paper',
-    xanchor: 'bottom',
-    yanchor: 'bottom',
-    x:
-      array
-        .slice(0, groupIndex)
-        .reduce((lastIndex, b) => lastIndex + b.data.length, 0) +
-      (group.data.length - 1) * 0.5,
-    y: 1.01,
-    text:
-      group.indel.substring(group.indel.length - 3, group.indel.length) !==
-      'tra'
-        ? group.indel
-            .substring(group.indel.length - 3, group.indel.length)
-            .charAt(0)
-            .toUpperCase() +
-          group.indel
-            .substring(group.indel.length - 3, group.indel.length)
-            .slice(1)
-        : 'T',
-    showarrow: false,
-    font: {
-      size: 14,
-      color: 'white',
-    },
-    align: 'center',
-  }));
-  const topShapeCluster = {
-    type: 'rect',
-    xref: 'x',
-    yref: 'paper',
-    x0: -0.4,
-    x1: 15.4,
-    y0: 1.14,
-    y1: 1.08,
-    fillcolor: '#808080',
-    line: {
-      width: 0,
-    },
-  };
-  const topShapeClusterAnnotation = {
-    xref: 'x',
-    yref: 'paper',
-    xanchor: 'bottom',
-    yanchor: 'bottom',
-    x: 7.5,
-    y: 1.08,
-    text: `Clustered`,
-    showarrow: false,
-    font: {
-      size: 14,
-      color: 'white',
-    },
-    align: 'center',
-  };
-  const topShapeNonluster = {
-    type: 'rect',
-    xref: 'x',
-    yref: 'paper',
-    x0: 15.6,
-    x1: 31.4,
-    y0: 1.14,
-    y1: 1.08,
-    fillcolor: '#000000',
-    line: {
-      width: 0,
-    },
-  };
-  const topShapeNonClusterAnnotation = {
-    xref: 'x',
-    yref: 'paper',
-    xanchor: 'bottom',
-    yanchor: 'bottom',
-    x: 23.5,
-    y: 1.08,
-    text: `Non-Clustered`,
-    showarrow: false,
-    font: {
-      size: 14,
-      color: 'white',
-    },
-    align: 'center',
-  };
-
-  const sampleAnnotation = {
-    xref: 'paper',
-    yref: 'paper',
-    xanchor: 'bottom',
-    yanchor: 'bottom',
-    x: 0.01,
-    y: 0.88,
-    text: '<b>' + sample + '</b>',
-    showarrow: false,
-    font: {
-      size: 18,
-      family: 'Arial',
-    },
-    align: 'center',
-  };
   const layout = {
     hoverlabel: { bgcolor: '#FFF' },
     height: 500,
@@ -312,15 +153,15 @@ export default function CN48(rawData, sample) {
     xaxis: {
       showticklabels: true,
       showline: true,
-      //tickangle: -90,
+      tickangle: -90,
       //tickfont: { size: 11 },
       tickmode: 'array',
 
       linecolor: 'black',
       linewidth: 1,
       mirror: 'all',
-      // tickvals: mutationTypeNames.map((_, i) => i),
-      // ticktext: mutationTypeNames.map((e) => e.mutationType),
+      tickvals: mutationTypeNames.map((_, i) => i),
+      ticktext: mutationTypeNames.map((e) => e.mutationType),
     },
     yaxis: {
       title: {
@@ -339,12 +180,12 @@ export default function CN48(rawData, sample) {
     },
 
     shapes: [...topShapes],
-    annotations: [
-      ...topShapeAnnitations,
-      topShapeClusterAnnotation,
-      topShapeNonClusterAnnotation,
-      sampleAnnotation,
-    ],
+    // annotations: [
+    //   //...topShapeAnnitations,
+    //   //topShapeClusterAnnotation,
+    //   //topShapeNonClusterAnnotation,
+    //   //sampleAnnotation,
+    // ],
   };
 
   return { traces, layout };
