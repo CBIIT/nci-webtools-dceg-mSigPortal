885578f036db2ae1efb783a4ee96d74316034962
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2022-09-15 11:56:09 -0400

    remove custome hover ms prevelence

--- client/src/components/controls/plotly/msPrevalence/msPrevalence.js
+++ client/src/components/controls/plotly/msPrevalence/msPrevalence.js
@@ -153,13 +153,13 @@ export default function MSPrevalence(groupBySignature, mutation) {
     textposition: 'outside',
     xaxis: 'x2',
     yaxis: 'y2',
-    //hoverinfo: 'x2+y2',
-    hovertemplate:
-      '<b> signatureName: ' +
-      '</b>' +
-      '%{customdata.signatureName}<br>' +
-      '<b>Frequency: </b>' +
-      '%{y:.1%}',
+    hoverinfo: 'x2+y2',
+    // hovertemplate:
+    //   '<b> signatureName: ' +
+    //   '</b>' +
+    //   '%{customdata.signatureName}<br>' +
+    //   '<b>Frequency: </b>' +
+    //   '%{y:.1%}',
     showlegend: false,
     domain: {
       row: 0,
