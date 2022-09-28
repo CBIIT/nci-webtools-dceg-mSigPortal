541975702d828495228634532a5b674a6617b000
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2022-09-28 16:21:47 -0400

    generate random unique colors for MS Prevelence that not in SBS, DBS, and ID

--- client/src/components/controls/plotly/msPrevalence/msPrevalence.js
+++ client/src/components/controls/plotly/msPrevalence/msPrevalence.js
@@ -1,4 +1,31 @@
-export default function MSPrevalence(groupBySignature, mutation) {
+import { groupBy } from 'lodash';
+
+export default function MSPrevalence(data, minimum) {
+  // calculate median burden across cancer types
+  const groupBySignature = groupBy(data, 'signatureName');
+
+  const dataResult = Object.entries(groupBySignature)
+    .map(([signatureName, data]) => {
+      const samples = data
+        .filter((e) => e.exposure)
+        .sort((a, b) => a.burden - b.burden);
+
+      const burdens = samples.filter((e) => e.burden).map((e) => e.burden);
+
+      const medianBurden =
+        burdens.length % 2 == 0
+          ? (burdens[burdens.length / 2] + burdens[burdens.length / 2 - 1]) / 2
+          : burdens[Math.floor(burdens.length / 2)];
+
+      return {
+        signatureName,
+        samples,
+        medianBurden,
+        totalSamples: data.length,
+      };
+    })
+    .filter((e) => e.medianBurden)
+    .sort((a, b) => a.medianBurden - b.medianBurden);
   // groupBySignature.sort(
   //   (a, b) =>
   //     a.samples.reduce((a, b) => a + b.exposure, 0) -
@@ -6,114 +33,149 @@ export default function MSPrevalence(groupBySignature, mutation) {
   // );
 
   let minumumNumber = 100;
-  mutation === null || mutation === undefined
+  minimum === null || minimum === undefined
     ? (minumumNumber = 100)
-    : (minumumNumber = parseInt(mutation));
+    : (minumumNumber = parseInt(minimum));
 
-  groupBySignature.sort(
+  dataResult.sort(
     (a, b) =>
       b.samples.filter((e) => e.exposure >= minumumNumber).length /
         b.totalSamples -
       a.samples.filter((e) => e.exposure >= minumumNumber).length /
         a.totalSamples
   );
+  console.log(dataResult);
+  const defaultNames = ['SBS', 'DBS', 'ID'];
+  const names = dataResult.map((group) => group.signatureName);
+  const longest = names.reduce((a, e) => (a > e.length ? a : e.length), 0);
+  const extraMargin = longest < 10 ? 60 : longest * 7;
 
-  console.log(groupBySignature);
-  const colors = {
-    1: '#4a9855',
-    2: '#e2a8ab',
-    3: '#40004b',
-    4: '#5aa1ca',
-    5: '#305d39',
-    6: '#785940',
-    '7a': '#6e70b7',
-    '7b': '#ff7f00',
-    '7c': '#fec44f',
-    '7d': '#846a2a',
-    8: '#cab2d6',
-    9: '#f4a582',
-    '10a': '#8dd3c7',
-    '10b': '#5e4fa2',
-    '10c': '#761429',
-    11: '#9e0142',
-    12: '#ffed6f',
-    13: '#e41a1c',
-    14: '#ffffbf',
-    15: '#4d4d4d',
-    16: '#513276',
-    '17a': '#df4c7d',
-    '17b': '#08519c',
-    18: '#b3de69',
-    19: '#dfc27d',
-    20: '#b2182b',
-    21: '#9ecae1',
-    22: '#01665e',
-    23: '#d53e4f',
-    24: '#1c9099',
-    25: '#35978f',
-    26: '#ec7014',
-    27: '#f46d43',
-    28: '#de77ae',
-    29: '#fdae61',
-    30: '#d9d9d9',
-    31: '#f781bf',
-    32: '#dd1c77',
-    33: '#b25d7e',
-    34: '#fee08b',
-    35: '#fc8d59',
-    36: 'yellow',
-    37: '#e6f598',
-    38: '#abdda4',
-    39: '#636363',
-    40: '#b15928',
-    41: '#fccde5',
-    42: '#ae017e',
-    43: '#66c2a5',
-    44: '#8c6bb1',
-    45: '#3288bd',
-    46: '#e6f598',
-    47: '#bababa',
-    48: '#5e4fa2',
-    49: '#40004b',
-    50: '#762a83',
-    51: '#9970ab',
-    52: '#c2a5cf',
-    53: '#e7d4e8',
-    54: '#fcc5c0',
-    55: '#d9f0d3',
-    56: '#8c510a',
-    57: '#a6dba0',
-    58: '#5aae61',
-    59: '#1b7837',
-    60: '#00441b',
-    84: '#063C3C',
-    85: '#AA9139',
-    92: '#0E1844',
-    '-others': '#cececa',
-  };
+  console.log(names);
+  console.log(longest);
+  console.log(extraMargin);
+
+  const contains = defaultNames.some((element) => {
+    if (names[0].includes(element)) {
+      return true;
+    }
+
+    return false;
+  });
+  console.log(contains);
+
+  let colors = {};
+
+  if (!contains) {
+    var randomColors = [];
+    while (randomColors.length < names.length) {
+      do {
+        var color = Math.floor(Math.random() * 1000000000 + 1);
+      } while (randomColors.indexOf(color) >= 0);
+      randomColors.push('#' + ('000000' + color.toString(16)).slice(-6));
+    }
+    console.log(randomColors);
+    names.forEach((element, index) => {
+      colors[element] = randomColors[index];
+    });
+    console.log(colors);
+  } else {
+    colors = {
+      1: '#4a9855',
+      2: '#e2a8ab',
+      3: '#40004b',
+      4: '#5aa1ca',
+      5: '#305d39',
+      6: '#785940',
+      '7a': '#6e70b7',
+      '7b': '#ff7f00',
+      '7c': '#fec44f',
+      '7d': '#846a2a',
+      8: '#cab2d6',
+      9: '#f4a582',
+      '10a': '#8dd3c7',
+      '10b': '#5e4fa2',
+      '10c': '#761429',
+      11: '#9e0142',
+      12: '#ffed6f',
+      13: '#e41a1c',
+      14: '#ffffbf',
+      15: '#4d4d4d',
+      16: '#513276',
+      '17a': '#df4c7d',
+      '17b': '#08519c',
+      18: '#b3de69',
+      19: '#dfc27d',
+      20: '#b2182b',
+      21: '#9ecae1',
+      22: '#01665e',
+      23: '#d53e4f',
+      24: '#1c9099',
+      25: '#35978f',
+      26: '#ec7014',
+      27: '#f46d43',
+      28: '#de77ae',
+      29: '#fdae61',
+      30: '#d9d9d9',
+      31: '#f781bf',
+      32: '#dd1c77',
+      33: '#b25d7e',
+      34: '#fee08b',
+      35: '#fc8d59',
+      36: 'yellow',
+      37: '#e6f598',
+      38: '#abdda4',
+      39: '#636363',
+      40: '#b15928',
+      41: '#fccde5',
+      42: '#ae017e',
+      43: '#66c2a5',
+      44: '#8c6bb1',
+      45: '#3288bd',
+      46: '#e6f598',
+      47: '#bababa',
+      48: '#5e4fa2',
+      49: '#40004b',
+      50: '#762a83',
+      51: '#9970ab',
+      52: '#c2a5cf',
+      53: '#e7d4e8',
+      54: '#fcc5c0',
+      55: '#d9f0d3',
+      56: '#8c510a',
+      57: '#a6dba0',
+      58: '#5aae61',
+      59: '#1b7837',
+      60: '#00441b',
+      84: '#063C3C',
+      85: '#AA9139',
+      92: '#0E1844',
+      '-others': '#cececa',
+    };
+  }
 
   const tracesPie = {
     type: 'pie',
-    labels: groupBySignature.map((group) => group.signatureName),
-    values: groupBySignature.map((group) =>
+    labels: dataResult.map((group) => group.signatureName),
+    values: dataResult.map((group) =>
       group.samples.reduce((a, b) => a + b.exposure, 0)
     ),
     textposition: 'inside',
     texttemplate: '%{percent:.1%}',
     showlegend: false,
     marker: {
-      colors: groupBySignature.map(
-        (group) =>
-          colors[group.signatureName.replace(/^\D*/, '').replace(')', '')]
+      colors: dataResult.map((group) =>
+        contains
+          ? colors[group.signatureName.replace(/^\D*/, '').replace(')', '')]
+          : colors[group.signatureName]
       ),
     },
-    test: groupBySignature.map((group) =>
+    test: dataResult.map((group) =>
       group.signatureName.replace(/^\D*/, '').replace(')', '')
     ),
     direction: 'clockwise',
     sort: true,
     domain: { x: [0, 0.2] },
-    customdata: groupBySignature.map((e) => ({
+    customdata: dataResult.map((e) => ({
       label: e.signatureName,
       value: e.samples.reduce((a, b) => a + b.exposure, 0),
     })),
@@ -121,13 +183,15 @@ export default function MSPrevalence(groupBySignature, mutation) {
       '<b>Signature Name:</b>%{label}<br><b>Total sample: </b>%{value}<br>%{percent}<extra></extra>',
   };
   console.log(tracesPie);
-  const tracesBar = groupBySignature.map((group, groupIndex, array) => ({
+  const tracesBar = dataResult.map((group, groupIndex, array) => ({
     group: group,
     array: array,
     name: group.signatureName,
     type: 'bar',
     marker: {
-      color: colors[group.signatureName.replace(/^\D*/, '').replace(')', '')],
+      color: contains
+        ? colors[group.signatureName.replace(/^\D*/, '').replace(')', '')]
+        : colors[group.signatureName],
     },
     // x:
     //   group.signatureName.length > 10
@@ -227,17 +291,6 @@ export default function MSPrevalence(groupBySignature, mutation) {
     titleAnnotations = [...titleAnnotation];
   }
 
-  const names = groupBySignature.map(
-    (group) => group.signatureName
-    // group.signatureName.length > 10
-    //   ? group.signatureName.substring(0, 10) + '...'
-    //   : group.signatureName,
-  );
-  const longest = names.reduce((a, e) => (a > e.length ? a : e.length), 0);
-  const extraMargin = longest < 10 ? 60 : longest * 7;
-
-  console.log(names);
-  console.log(extraMargin);
   const layout = {
     grid: { rows: 1, columns: 2 },
     hoverlabel: { bgcolor: '#FFF' },
--- client/src/components/pages/exposure/msPrevalence/apiSlice.js
+++ client/src/components/pages/exposure/msPrevalence/apiSlice.js
@@ -1,6 +1,5 @@
 import { explorationApiSlice } from '../../../../services/store/rootApi';
 import MsPrevalence from '../../../controls/plotly/msPrevalence/msPrevalence';
-import { groupBy } from 'lodash';
 
 export const msPrevalenceApiSlice = explorationApiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -13,41 +12,11 @@ export const msPrevalenceApiSlice = explorationApiSlice.injectEndpoints({
       transformResponse: (data, meta, arg) => {
         console.log('prevalence api');
         console.log(data);
+        console.log(arg);
         const { minimum } = arg;
         console.log(minimum);
 
-        // calculate median burden across cancer types
-        const groupBySignature = groupBy(data, 'signatureName');
-        const groupBySample = groupBy(data, 'sample');
-
-        const transform = Object.entries(groupBySignature)
-          .map(([signatureName, data]) => {
-            const samples = data
-              .filter((e) => e.exposure)
-              .sort((a, b) => a.burden - b.burden);
-
-            const burdens = samples
-              .filter((e) => e.burden)
-              .map((e) => e.burden);
-
-            const medianBurden =
-              burdens.length % 2 == 0
-                ? (burdens[burdens.length / 2] +
-                    burdens[burdens.length / 2 - 1]) /
-                  2
-                : burdens[Math.floor(burdens.length / 2)];
-
-            return {
-              signatureName,
-              samples,
-              medianBurden,
-              totalSamples: data.length,
-            };
-          })
-          .filter((e) => e.medianBurden)
-          .sort((a, b) => a.medianBurden - b.medianBurden);
-
-        return MsPrevalence(transform, minimum);
+        return MsPrevalence(data, minimum);
         //return MsPrevalence(transform, groupBySample);
       },
     }),
