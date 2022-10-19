51d5be7e6c7dbc31b941f444de5dbf7ac0b3f714
Cho <phyllip3@users.noreply.github.com>
2022-10-19 14:03:14 -0400

    adjust tmb size and labels

--- client/src/components/controls/plotly/tmb/tmb.js
+++ client/src/components/controls/plotly/tmb/tmb.js
@@ -1,12 +1,4 @@
-import { faCommentDollar } from '@fortawesome/free-solid-svg-icons';
-
 export default function TMB(data, tmbTabName, signatureName) {
-  function average(arr) {
-    const sum = arr.reduce((a, b) => a + b, 0);
-    return sum / arr.length || 0;
-  }
-  //console.log(data);
-
   const totalCancer = data.length;
 
   const absYValue = data
@@ -24,7 +16,6 @@ export default function TMB(data, tmbTabName, signatureName) {
       sampleName: e.sample,
     })),
     y: element.samples.map((e) => e.burden),
-    // average: average(element.samples.map((e) => e.tmb)),
     hovertemplate:
       tmbTabName === 'TMBSignature'
         ? `<b>Sample Name:</b> %{customdata.sampleName}<br><b>Signature Name</b>: ${element.signatureName}` +
@@ -46,6 +37,7 @@ export default function TMB(data, tmbTabName, signatureName) {
       color: ['blue'],
     },
     name: 'Available Samples',
+    hoverinfo: 'skip',
   };
 
   const traceLabelGreen = {
@@ -57,26 +49,17 @@ export default function TMB(data, tmbTabName, signatureName) {
       color: ['green'],
     },
     name: 'Total Samples',
+    hoverinfo: 'skip',
   };
 
-  const topLabel = data.map((element, index, array) => ({
+  const groupLabel = data.map((element, index, array) => ({
     element: element,
     xref: 'x',
     yref: 'paper',
     xanchor: 'left',
     yanchor: 'top',
-    //x: array.length > 1 ? index : (index + index + 1) * 0.5,
-    x: index,
-    //y: 1.01,
+    x: index + 0.2,
     y: 0,
-    // text:
-    //   tmbTabName === 'TMBSignature' && element.signatureName.length < 13
-    //     ? element.signatureName
-    //     : tmbTabName === 'TMBSignature' && element.signatureName.length > 13
-    //     ? element.signatureName.substring(0, 13) + '...'
-    //     : tmbTabName !== 'TMBSignature' && element.cancer.length < 13
-    //     ? element.cancer
-    //     : element.cancer.substring(0, 13) + '...',
     text:
       tmbTabName === 'TMBSignature' ? element.signatureName : element.cancer,
     showarrow: false,
@@ -84,10 +67,10 @@ export default function TMB(data, tmbTabName, signatureName) {
       //size: 12,
     },
     align: 'left',
-    textangle: 60,
+    textangle: totalCancer > 4 ? 60 : 0,
   }));
 
-  const bottoLabel1 = data.map((element, index, array) => ({
+  const groupCountAnnotation = data.map((element, index, array) => ({
     xref: 'x',
     yref: 'paper',
     xanchor: 'bottom',
@@ -106,14 +89,12 @@ export default function TMB(data, tmbTabName, signatureName) {
     align: 'center',
   }));
 
-  const bottoLabelline = data.map((element, index, array) => ({
+  const countDivider = data.map((element, index, array) => ({
     type: 'line',
     xref: 'x',
     yref: 'paper',
     x0: index + 0.4,
     x1: index + 0.6,
-    //y0: -0.07,
-    //y1: -0.07,
     y0: 1.1,
     y1: 1.1,
     line: {
@@ -122,14 +103,14 @@ export default function TMB(data, tmbTabName, signatureName) {
     },
   }));
 
-  const bottoLabel2 = data.map((element, index, array) => ({
+  const totalCountAnnotation = data.map((element, index, array) => ({
     xref: 'x',
     yref: 'paper',
     xanchor: 'bottom',
     yanchor: 'bottom',
     x: (index + index + 1) * 0.5,
     //y: -0.14,
-    y: 1.01,
+    y: 1.05,
     text: `${element.totalSamples}`,
     showarrow: false,
     font: {
@@ -174,7 +155,7 @@ export default function TMB(data, tmbTabName, signatureName) {
     xanchor: 'bottom',
     yanchor: 'bottom',
     x: 0.01,
-    y: 0.895,
+    y: 0.9,
     text: signatureName,
     showarrow: false,
     font: {
@@ -186,24 +167,27 @@ export default function TMB(data, tmbTabName, signatureName) {
   let annotations = [];
   signatureName != null
     ? (annotations = [
-        ...topLabel,
-        ...bottoLabel1,
-        ...bottoLabel2,
+        ...groupLabel,
+        ...groupCountAnnotation,
+        ...totalCountAnnotation,
         signatureNameAnnotation,
       ])
-    : (annotations = [...topLabel, ...bottoLabel1, ...bottoLabel2]);
+    : (annotations = [
+        ...groupLabel,
+        ...groupCountAnnotation,
+        ...totalCountAnnotation,
+      ]);
 
   // find the longest label to calculate extra height margin
-
-  const labels = topLabel.map((e) => e.text);
+  const labels = groupLabel.map((e) => e.text);
   const longest = labels.reduce((a, e) => (a > e.length ? a : e.length), 0);
-  const extraMargin = longest < 10 ? 60 : longest * 7;
-  ///console.log(labels);
+  const extraMargin = longest < 10 ? 60 : longest * 7.5;
+
   const layout = {
-    width: totalCancer > 1 ? null : 350,
+    width: totalCancer > 4 ? null : 300 + 200 * totalCancer - 1,
     autosize: true,
-    height: 500,
-    legend: { orientation: 'h', x: 0.25, y: 1.35 },
+    height: 500 + extraMargin,
+    legend: { orientation: 'h', x: 0.25, y: 1.25 },
     xaxis: {
       showticklabels: false,
       tickfont: {
@@ -216,8 +200,6 @@ export default function TMB(data, tmbTabName, signatureName) {
       mirror: true,
       tickmode: 'array',
       showgrid: false,
-      //tickvals: flatSorted.map((_, i) => i),
-      //ticktext: flatSorted.map((_, i) => i),
     },
     yaxis: {
       title: 'Number of Mutations per Megabase<br>(log10)',
@@ -234,7 +216,7 @@ export default function TMB(data, tmbTabName, signatureName) {
       b: extraMargin,
       t: 140,
     },
-    shapes: [...shapes, ...lines, ...bottoLabelline],
+    shapes: [...shapes, ...lines, ...countDivider],
     annotations: annotations,
   };
 
@@ -247,5 +229,4 @@ export default function TMB(data, tmbTabName, signatureName) {
     layout: layout,
     config,
   };
-  //return { traces, layout };
 }
