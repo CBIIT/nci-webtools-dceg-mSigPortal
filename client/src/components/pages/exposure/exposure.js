98a93d784107f301cfbc1fe052af97ca745b7b55
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-01-12 17:25:43 -0500

    bring the submenu of exploration to middle

--- client/src/components/pages/exposure/exposure.js
+++ client/src/components/pages/exposure/exposure.js
@@ -650,12 +650,15 @@ export default function Exposure({ match }) {
 
   return (
     <div className="position-relative">
-      <div className="mx-3">
-        <div className="mx-3 bg-white border border-top-0">
+      <div className="bg-white border border-top-0">
+        <div className="container">
           {/* for desktops and tablets */}
           <div className="container">
             <div className="d-none d-md-block">
-              <Nav defaultActiveKey="profilerSummary">
+              <Nav
+                defaultActiveKey="profilerSummary"
+                className="container pl-3"
+              >
                 {tabs.map(({ name, id }) => {
                   if (name)
                     return (
@@ -721,6 +724,7 @@ export default function Exposure({ match }) {
           </div>
         </div>
       </div>
+
       <SidebarContainer
         className="m-3"
         collapsed={!openSidebar}
