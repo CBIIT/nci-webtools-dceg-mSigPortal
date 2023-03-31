cd46a8eeb072152461024f31f38fe52d1e3cf9ce
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-03-31 16:06:20 -0400

    add local/fargate workers

---
+++ server/services/workers.js
@@ -0,0 +1,69 @@
+import path from 'path';
+import ECS, { ECSClient, RunTaskCommand } from '@aws-sdk/client-ecs';
+import { readJson } from './utils.js';
+import { createLogger } from '../services/logger.js';
+import { profilerExtraction } from './api/visualization/profilerExtraction.js';
+
+export function getWorker(workerType = 'local') {
+  switch (workerType) {
+    case 'local':
+      return runLocalWorker;
+    case 'fargate':
+      return runFargateWorker;
+    default:
+      throw new Error(`Unknown worker type: ${workerType}`);
+  }
+}
+
+/**
+ * Executes a worker process locally.
+ * @param {string} id
+ * @param {string} app
+ * @param {string} env
+ * @returns
+ */
+export async function runLocalWorker(id, app, env = process.env) {
+  const paramsFilePath = path.resolve(env.INPUT_FOLDER, id, 'params.json');
+  const params = await readJson(paramsFilePath);
+  const logger = app.locals.logger;
+  const dbConnection = app.locals.sqlite(id, 'local');
+  return await profilerExtraction(params, logger, dbConnection, env);
+}
+
+/**
+ * Executes a worker process in an AWS Fargate task.
+ * @param {string} id
+ * @param {string} env
+ * @returns {Promise<ECS.RunTaskCommandOutput>} task output
+ */
+export async function runFargateWorker(id, env = process.env) {
+  const { ECS_CLUSTER, SUBNET_IDS, SECURITY_GROUP_IDS, WORKER_TASK_NAME } = env;
+  const client = new ECSClient();
+  const workerCommand = ['node', '--require', 'dotenv/config', 'worker.js', id];
+  const logger = createLogger(env.APP_NAME, env.LOG_LEVEL);
+  const taskCommand = new RunTaskCommand({
+    cluster: ECS_CLUSTER,
+    count: 1,
+    launchType: 'FARGATE',
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        securityGroups: SECURITY_GROUP_IDS.split(','),
+        subnets: SUBNET_IDS.split(','),
+      },
+    },
+    taskDefinition: WORKER_TASK_NAME,
+    overrides: {
+      containerOverrides: [
+        {
+          name: 'worker',
+          command: workerCommand,
+        },
+      ],
+    },
+  });
+  const response = await client.send(taskCommand);
+  logger.info('Submitted Fargate RunTask command');
+  logger.info(workerCommand);
+  logger.info(response);
+  return response;
+}
