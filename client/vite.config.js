8014d77181bc6cca5526e1b24326cfa7546fb678
Phyllip Cho <phyllip3@users.noreply.github.com>
2024-08-30 13:04:00 -0400

    use base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'mutational-signatures',
   server: {
     port: 3000,
     proxy: {
