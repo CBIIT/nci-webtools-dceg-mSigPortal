f9e166b06b3d232d2ede71e6600761c37b8df10c
Phyllip Cho <phyllip3@users.noreply.github.com>
2024-09-03 11:01:11 -0400

    update proxy config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+const BASE_URL = '/mutational-signatures';
 export default defineConfig({
   plugins: [react()],
-  base: 'mutational-signatures',
+  base: BASE_URL,
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:8330',
-      '/extraction': 'http://localhost:8332',
+      [`${BASE_URL}/api`]: {
+        target: 'http://localhost:8330',
+        rewrite: (path) => path.replace(BASE_URL, ''),
+      },
+      [`${BASE_URL}/extraction`]: {
+        target: 'http://localhost:8332',
+        rewrite: (path) => path.replace(BASE_URL, ''),
+      },
     },
   },
   define: {
