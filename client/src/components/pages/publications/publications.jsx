387df8ed073ec6cb30a9c97a9734af97d574749f
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2024-10-31 14:56:25 -0400

    Add journal column to original research paper

--- client/src/components/pages/publications/publications.jsx
+++ client/src/components/pages/publications/publications.jsx
@@ -99,6 +99,7 @@ export default function Publications() {
                     'programmingLanguage',
                     'firstAuthor',
                     'lastAuthor',
+                    'journal',
                     'bioRxivOrPubmedId',
                     'doi',
                     'sourceUrl',
