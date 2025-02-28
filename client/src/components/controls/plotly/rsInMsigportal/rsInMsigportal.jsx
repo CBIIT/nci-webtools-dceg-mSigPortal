731a13fa8c7cd7b226b0f62ef484c52d4b94468f
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2025-02-28 16:08:26 -0500

    Update piechart

--- client/src/components/controls/plotly/rsInMsigportal/rsInMsigportal.jsx
+++ client/src/components/controls/plotly/rsInMsigportal/rsInMsigportal.jsx
@@ -13,12 +13,16 @@ export default function RsInMsigportal(rawData) {
   ];
   const profile_order_1 = ['SBS96', 'DBS78', 'ID83'];
   const groupBySpecies = groupBy(rawData, (item) => `${item.species}`);
+  console.log("rawData in RsInMsigportal: ", rawData);
   const groupByignatureSetName = groupBy(
     rawData,
     (item) => `${item.signatureSetName}`
   );
 
+  console.log("groupByignatureSetName ", groupByignatureSetName);
+
   const signatureSetName = Object.keys(groupByignatureSetName).map((e) => e);
+  console.log("signatureSetName ", signatureSetName);
 
   var randomColors = [];
   while (randomColors.length < signatureSetName.length) {
@@ -34,18 +38,30 @@ export default function RsInMsigportal(rawData) {
   let dataHuman = [];
   let dataMm9 = [];
   let dataRn6 = [];
+  let dataGallus = [];
   Object.entries(groupBySpecies).map(([key, val], index) => {
     if (key.includes('(GRCh37/38)')) {
       dataHuman.push(val);
     } else if (key.includes('(mm9/10)')) {
       dataMm9.push(val);
-    } else {
+    } else if (key.includes('(rn6)')) {
       dataRn6.push(val);
+    }    
+     else {
+      dataGallus.push(val);
     }
   });
+
+  console.log("groupBySpecies ", groupBySpecies);
+  console.log("dataHuman ", dataHuman);
+  console.log("dataMm9 ", dataMm9);
+  console.log("dataRn6 ", dataRn6);
+  console.log("dataGallus ", dataGallus);
+
   dataHuman = dataHuman.flat();
   dataMm9 = dataMm9.flat();
   dataRn6 = dataRn6.flat();
+  dataGallus = dataGallus.flat();
 
   const groupedHuman = groupBy(
     dataHuman,
@@ -57,6 +73,10 @@ export default function RsInMsigportal(rawData) {
   );
   const groupedRn6 = groupBy(
     dataRn6,
+    (item) => `${item.profile}${item.matrix}`
+  );
+  const groupedGallus = groupBy(
+    dataGallus,
     (item) => `${item.profile}${item.matrix}`
   );
 
@@ -87,7 +107,8 @@ export default function RsInMsigportal(rawData) {
             ? Math.round((index * (1 / 5) + 0.2) * 10) / 10
             : Math.round(((index - 4) * (1 / 5) + 0.2) * 10) / 10,
         ],
-        y: [index < 4 ? 0.55 : 0.8, index < 4 ? 0.725 : 0.975],
+        // y: [index < 4 ? 0.675 : 0.85, index < 4 ? 0.8 : 0.975],
+        y: [index < 4 ? 0.635 : 0.83, index < 4 ? 0.79 : 0.985],
       },
       hovertemplate:
         '<b>%{label}</b> <br>%{percent} </br> %{value}  <extra></extra>',
@@ -119,7 +140,7 @@ export default function RsInMsigportal(rawData) {
             ? Math.round((index * (1 / 5) + 0.2) * 10) / 10
             : Math.round(((index - 4) * (1 / 5) + 0.2) * 10) / 10,
         ],
-        y: [0.265, 0.44],
+        y: [0.42, 0.575],
       },
       hovertemplate:
         '<b>%{label}</b> <br>%{percent} </br> %{value}  <extra></extra>',
@@ -152,7 +173,39 @@ export default function RsInMsigportal(rawData) {
             ? Math.round((index * (1 / 5) + 0.2) * 10) / 10
             : Math.round(((index - 4) * (1 / 5) + 0.2) * 10) / 10,
         ],
-        y: [0, 0.175],
+        y: [0.205, 0.36],
+      },
+      hovertemplate:
+        '<b>%{label}</b> <br>%{percent} </br> %{value}  <extra></extra>',
+    })
+  );
+  const tracePies3 = Object.entries(groupedGallus).map(
+    ([key, element], index, array) => ({
+      type: 'pie',
+      e: element,
+      marker: {
+        color: element.map((e) => colors[e.signatureSetName]),
+        line: {
+          color: 'black',
+          width: 1,
+        },
+      },
+      textposition: 'inside',
+      labels: element.map((e) => e.signatureSetName),
+      values: element.map((e) => parseInt(e.count)),
+      texttemplate: '%{value}',
+      direction: 'clockwise',
+      name: key,
+      domain: {
+        x: [
+          index < 4
+            ? Math.round(index * (1 / 5) * 10) / 10
+            : Math.round((index - 4) * (1 / 5) * 10) / 10,
+          index < 4
+            ? Math.round((index * (1 / 5) + 0.2) * 10) / 10
+            : Math.round(((index - 4) * (1 / 5) + 0.2) * 10) / 10,
+        ],
+        y: [0, 0.155],
       },
       hovertemplate:
         '<b>%{label}</b> <br>%{percent} </br> %{value}  <extra></extra>',
@@ -196,7 +249,7 @@ export default function RsInMsigportal(rawData) {
       align: 'left',
       //x: index < 4 ? index * (1 / 5) + 0.0975 : (index - 4) * (1 / 5) + 0.0975,
       x: indexPos(index),
-      y: index < 4 ? 0.74 : 0.99,
+      y: index < 4 ? 0.805 : 0.995,
     })
   );
   const pieTitles1 = Object.entries(groupedMm9_sorted).map(
@@ -213,7 +266,7 @@ export default function RsInMsigportal(rawData) {
       align: 'center',
       // x: index < 4 ? index * (1 / 5) + 0.085 : (index - 4) * (1 / 5) + 0.085,
       x: indexPos(index),
-      y: 0.455,
+      y: 0.59,
     })
   );
 
@@ -228,7 +281,22 @@ export default function RsInMsigportal(rawData) {
       align: 'center',
       //x: index < 4 ? index * (1 / 5) + 0.08 : (index - 4) * (1 / 5) + 0.08,
       x: indexPos(index),
-      y: 0.19,
+      y: 0.375,
+    })
+  );
+
+  const pieTitles3 = Object.entries(groupedGallus).map(
+    ([key, element], index, array) => ({
+      xref: 'paper',
+      yref: 'paper',
+      xanchor: 'center',
+      yanchor: 'middle',
+      showarrow: false,
+      text: key.padStart(7, ' '),
+      align: 'center',
+      //x: index < 4 ? index * (1 / 5) + 0.08 : (index - 4) * (1 / 5) + 0.08,
+      x: indexPos(index),
+      y: 0.17,
     })
   );
   const annotationTitle0 = {
@@ -256,7 +324,7 @@ export default function RsInMsigportal(rawData) {
       size: 16,
     },
     x: 0.25,
-    y: 0.49,
+    y: 0.60,
   };
 
   const annotationTitle2 = {
@@ -270,7 +338,21 @@ export default function RsInMsigportal(rawData) {
       size: 16,
     },
     x: 0.15,
-    y: 0.22,
+    y: 0.385,
+  };
+
+  const annotationTitle3 = {
+    xref: 'paper',
+    yref: 'paper',
+    xanchor: 'bottom',
+    yanchor: 'bottom',
+    showarrow: false,
+    text: Object.keys(groupBySpecies)[3],
+    font: {
+      size: 16,
+    },
+    x: 0.15,
+    y: 0.18,
   };
 
   const shapes = [
@@ -305,9 +387,9 @@ export default function RsInMsigportal(rawData) {
       xref: 'paper',
       yref: 'paper',
       x0: 0,
-      y0: 0.5,
+      y0: 0.61,
       x1: 0.23,
-      y1: 0.5,
+      y1: 0.61,
       line: {
         color: 'gray',
         width: 3,
@@ -318,9 +400,9 @@ export default function RsInMsigportal(rawData) {
       xref: 'paper',
       yref: 'paper',
       x0: 0.5,
-      y0: 0.5,
+      y0: 0.61,
       x1: 0.7,
-      y1: 0.5,
+      y1: 0.61,
       line: {
         color: 'gray',
         width: 3,
@@ -331,9 +413,9 @@ export default function RsInMsigportal(rawData) {
       xref: 'paper',
       yref: 'paper',
       x0: 0,
-      y0: 0.23,
+      y0: 0.395,
       x1: 0.14,
-      y1: 0.23,
+      y1: 0.395,
       line: {
         color: 'gray',
         width: 3,
@@ -344,9 +426,35 @@ export default function RsInMsigportal(rawData) {
       xref: 'paper',
       yref: 'paper',
       x0: 0.38,
-      y0: 0.23,
+      y0: 0.395,
       x1: 0.7,
-      y1: 0.23,
+      y1: 0.395,
+      line: {
+        color: 'gray',
+        width: 3,
+      },
+    },
+    {
+      type: 'line',
+      xref: 'paper',
+      yref: 'paper',
+      x0: 0,
+      y0: 0.19,
+      x1: 0.14,
+      y1: 0.19,
+      line: {
+        color: 'gray',
+        width: 3,
+      },
+    },
+    {
+      type: 'line',
+      xref: 'paper',
+      yref: 'paper',
+      x0: 0.475,
+      y0: 0.19,
+      x1: 0.7,
+      y1: 0.19,
       line: {
         color: 'gray',
         width: 3,
@@ -354,7 +462,7 @@ export default function RsInMsigportal(rawData) {
     },
   ];
 
-  const traces = [...tracePies0, ...tracePies1, ...tracePies2];
+  const traces = [...tracePies0, ...tracePies1, ...tracePies2, ...tracePies3];
   const layout = {
     hoverlabel: { bgcolor: '#FFF' },
     height: 1080,
@@ -376,9 +484,11 @@ export default function RsInMsigportal(rawData) {
       ...pieTitles0,
       ...pieTitles1,
       ...pieTitles2,
+      ...pieTitles3,
       annotationTitle0,
       annotationTitle1,
       annotationTitle2,
+      annotationTitle3
     ],
     shapes: [...shapes],
   };
