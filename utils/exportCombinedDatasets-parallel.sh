7fcc5dc8df9cd07abdc954ab0b535401d2947aeb
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-08-25 15:31:05 -0400

    add parallel processing script

---
+++ utils/exportCombinedDatasets-parallel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+Rscript combineDatasets.R 'Association/*_*_*_vardata.RData' combineAssociationFiles Association/data.csv &
+Rscript combineDatasets.R 'Exposure/*_*_exposure_refdata.RData' combineExposureFiles Exposure/data.csv &
+Rscript combineDatasets.R 'Exposure/Study_Signatures/*_signature_refsets.RData' combineSignatureFiles Exposure/studySignaturesData.csv &
+Rscript combineDatasets.R 'Seqmatrix/*_*_seqmatrix_refdata.RData' combineSeqmatrixFiles Seqmatrix/data.csv &
+Rscript combineDatasets.R 'Signature/signature_refsets.RData' combineSignatureFiles Signature/data.csv &
+Rscript combineDatasets.R 'Signature/signature_refsets_summary.RData' signatureSummary Signature/summary.csv &
+Rscript combineDatasets.R 'Others/content_data_all.RData' combinePatternFiles Others/pattern.csv &
+Rscript combineDatasets.R 'Etiology/aetiology_exposure.RData' combineEtiology Etiology/etiology.csv &
+Rscript combineDatasets.R 'Etiology/aetiology_organ_specific_signature.RData' combineEtiologyOrgan Etiology/etiologyOrgan.csv &
+Rscript combineDatasets.R 'Etiology/signature_refsets_etiology.RData' combineEtiologySignature Etiology/etiologySignature.csv &
+Rscript combineDatasets.R 'Others/refgenome.RData' combineRefgenome Others/refgenome.csv &
+
+wait
