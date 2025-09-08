dc1b2f17520b857f0322e0e7848d84f7f66a1c7b
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2025-09-08 15:40:10 -0400

    remove SAT unneed file

--- client/src/components/controls/plotly/SATS/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SATSSignaturePresence } from './satsSignaturePresence.jsx';
-export { default as SATSDotPlot } from './satsDotPlot.jsx';
-export { default as SATSTMBBar } from './satsTMBBar.jsx';
