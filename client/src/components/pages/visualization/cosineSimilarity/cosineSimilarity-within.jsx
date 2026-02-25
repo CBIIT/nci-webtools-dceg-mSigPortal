e867678840599e110a3ddccc00b4f6dd8e6bb830
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2026-02-25 16:16:40 -0500

    add alert box into the error to match other tab

--- client/src/components/pages/visualization/cosineSimilarity/cosineSimilarity-within.jsx
+++ client/src/components/pages/visualization/cosineSimilarity/cosineSimilarity-within.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { Form, Row, Col, Button, Alert } from 'react-bootstrap';
 import Select from '../../../controls/select/selectHookForm';
 import { useForm } from 'react-hook-form';
 import { NavHashLink } from 'react-router-hash-link';
@@ -163,12 +163,12 @@ export default function CsWithin({ state }) {
         {(error || data?.output?.error || data?.output?.uncaughtError) && (
           <>
             <hr />
-            <p className="p-3">
+            <Alert variant="warning" className="mx-3">
               {error ||
                 data?.output?.error ||
                 data?.output?.uncaughtError ||
                 'An error has occurred. Please verify your input.'}
-            </p>
+            </Alert>
           </>
         )}
         {data?.output.plotPath && (
